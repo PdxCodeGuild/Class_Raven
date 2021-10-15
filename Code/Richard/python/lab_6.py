7f7b1ff94598707e6bb885c1a3bd222e8e5e8d7a
Richard <raw0039@gmail.com>
2021-10-14 23:22:09 -0500

    completed and cleaned up unnecessary lines.

--- Code/Richard/python/lab_6.py
+++ Code/Richard/python/lab_6.py
@@ -4,37 +4,24 @@ def card_check(card):
     card_list = []
     card_L = list(card)
     card_list = [int(i) for i in card_L] # learned method in search for "list of str to int"
-    '''for i in card_L:
-        int(i)
-        card_list.append(i)'''
-    print(card_list)
     check_digit = card_list.pop()
-    print(check_digit)
-    print(card_list)
     card_list.reverse()
-    print(card_list)
     double_list = []
     double_list2 = []
     for num in card_list:
         num += num
-        double_list.append(num)
-    print(double_list)    
+        double_list.append(num)   
     for i in double_list:
         if i > 9:
             i -= 9
             double_list2.append(i)
         else:
             double_list2.append(i)
-    print(double_list2)
     Sum = str(sum(double_list2)) # search for how to use sum function with lists.
-    print(Sum)
     Sum_list = list(Sum)
-    print(Sum_list)
     if int(Sum_list[1]) == check_digit:
-        print(True)
         return True
     else:
-        print(False)
         return False
 
 test= input("input credit card number to be validated\n>")
