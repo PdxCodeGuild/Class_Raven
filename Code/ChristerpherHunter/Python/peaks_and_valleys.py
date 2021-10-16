c057d399cd1df8f20b1460dd257cf616c103beda
Christerpher Hunter <djhunter67@gmail.com>
2021-10-16 09:34:00 -0400

    Proceeding to hard code the peaks and valleys...

--- Code/ChristerpherHunter/Python/peaks_and_valleys.py
+++ Code/ChristerpherHunter/Python/peaks_and_valleys.py
@@ -25,21 +25,24 @@ def valleys(data: list) -> list:
 
 def peaks_and_valleys(data: list) -> list:
 
-    for i, val in enumerate(data):
-        print("x" * i)
-        print("x" * val)
+    # for i, val in enumerate(data):
+
+    print(" " * 14 + "x" + " " * 5 + "x")  # 9 High
+    print(" " * 13 + "x" * 3 + " " * 3 + "x" * 2)  # 8 High
+    print(" " * 5 + "x" + " " * 6 + "x" * 5 + " " + "x" * 3)  # 7 High
+    print(" " * 5 + "x" * 3)  # High 6
 
 
 def main() -> None:
 
     data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
-    val = peaks(data)
-    val2 = valleys(data)
+    # val = peaks(data)
+    # val2 = valleys(data)
 
     peaks_and_valleys(data)
 
-    print(val, val2)
+    # print(val, val2)
 
 
 if __name__ == "__main__":
