6fe5a8b3ef6e1740dde4cdc5e579b5e0234c2d25
SGTWOBBLE <aaron.quiroz23@yahoo.com>
2021-10-15 14:05:03 -1000

    lab3 v1_v2 complete

--- Code/Aaron/Python/lab3.py
+++ Code/Aaron/Python/lab3.py
@@ -1,6 +1,14 @@
 nums = [5, 0, 8, 3, 4, 1, 6]
 
-average = sum(nums) / len(nums)
+# initialize a variable to hold the 'running sum'
+total = 0
 
-print("average median is: " + str(round(average)))
+# iterate through the list keeping a 'running sum'
+for num in nums:
+    total = total + num
 
+# get the average
+average = total / len(nums)
+
+# print average rounded to two decimal places
+print("Average:", round(average))
