6db04e8d4fb398205fe24189f8b8ef33ee54aa4e
Rafael <realworldsurf@gmail.com>
2021-10-15 20:42:04 -0700

    incomplete Lab_06 push

---
+++ Code/Rafael/Python/Lab_06.py
@@ -0,0 +1,21 @@
+# Lab_06 Credit Card Validation
+
+
+#def validate(digit):
+
+# Converts input into a list and sperates each character with
+
+
+user_input = list(input('Enter 16 single digits from 0-9 with no spaces: ')) #.strip()
+
+
+pop = user_input.pop()
+user_input.reverse()
+
+
+print(user_input)
+
+
+
+
+#numbers = validate(user_input)
