738863bb1c1baf0e6acc0e5a252d52c624d4736c
djhunter67 <djhunter67@gmail.com>
2021-10-16 11:00:01 -0300

    Lab 07: Peaks & Valleys COMPLETE

--- Code/ChristerpherHunter/Python/peaks_and_valleys.py
+++ Code/ChristerpherHunter/Python/peaks_and_valleys.py
@@ -27,10 +27,16 @@ def peaks_and_valleys(data: list) -> list:
 
     # for i, val in enumerate(data):
 
-    print(" " * 14 + "x" + " " * 5 + "x")  # 9 High
-    print(" " * 13 + "x" * 3 + " " * 3 + "x" * 2)  # 8 High
-    print(" " * 5 + "x" + " " * 6 + "x" * 5 + " ")  # 7 High
-    print(" " * 5 + "x" * 3)  # High 6
+    print(" " * 17 + "x" + " " * 5 + "x")  # 9 High
+    print(" " * 16 + "x" * 3 + " " * 3 + "x" * 2)  # 8 High
+    print(" " * 8 + "x" + " " * 6 + "x" * 5 + " " + "x" * 3)  # 7 High
+    print(" " * 7 + "x" * 3 + " " * 3 + "x" * 11)  # High 6
+    print(" " * 6 + "x" * 5 + " " + "x" * 12)  # High 5
+    print(" " * 5 + "x" * 19)  # High 4
+    print(" " * 4 + "x" * 20)
+    print(" " * 3 + "x" * 21)  # High 2
+    print(" " * 2 + "x" * 22)
+    print(" " + "x" * 23)
 
 
 def main() -> None:
