904e92d83303b02a5bf1cbcb13a1aa19d86c6740
Christerpher Hunter <djhunter67@gmail.com>
2021-10-17 13:27:05 -0400

    COMPLETE

--- Code/ChristerpherHunter/Python/blackjack_advice.py
+++ Code/ChristerpherHunter/Python/blackjack_advice.py
@@ -18,46 +18,48 @@ class Blackjacked:
     def grade_cards(self) -> None:
         """Assign value to the given cards"""
 
-        match self.__card_1 | self.__card_2 | self.__card_3:
-            case 'A':
-                if self.total > 11:
-                    self.total += 1
-                else:
-                    self.total += 11
-            case 'K' | 'J' | '10':
-                self.total += 10
-            case '9':
-                self.total += 9
-            case '8':
-                self.total += 8
-            case '7':
-                self.total += 7
-            case '6':
-                self.total += 6
-            case '5':
-                self.total += 5
-            case '4':
-                self.total += 4
-            case '3':
-                self.total += 3
-            case '2':
-                self.total += 2
-            case '1':
-                self.total += 1
-            case 'A':
-                self.total += 0
-            case _:
-                print("Invalid input")
+        for card in [self.__card_3, self.__card_2, self.__card_1]:
 
-        if self.__card_1 | self.__card_2 | self.__card_3 == 'A':
-            if self.total > 11:
-                self.total += 1
+            match card:
+                case 'A':
+                    if self.total >= 11:
+                        self.total += 1
+                    else:
+                        self.total += 11
+                case 'K' | 'J' | 'Q' | '10':
+                    self.total += 10
+                case '9':
+                    self.total += 9
+                case '8':
+                    self.total += 8
+                case '7':
+                    self.total += 7
+                case '6':
+                    self.total += 6
+                case '5':
+                    self.total += 5
+                case '4':
+                    self.total += 4
+                case '3':
+                    self.total += 3
+                case '2':
+                    self.total += 2
+                case '1':
+                    self.total += 1
+                case 'A':
+                    self.total += 0
+                case _:
+                    print("Invalid input")
+                    exit()
 
     def probability(self) -> None:
         """Determine the probability"""
 
-    def advice(self) -> None:
+    def adviced(self) -> None:
         """Make an informed decision"""
+
+        if self.total > 21 and self.__card_2 == 'A':
+            self.total -= 10
 
         match self.total:
             case 21:
@@ -74,8 +76,30 @@ class Blackjacked:
                 self.advice = "Stay"
             case 15:
                 self.advice = "Stay"
-            case _:
+            case 14:
                 self.advice = "Hit"
+            case 13:
+                self.advice = "Hit"
+            case 12:
+                self.advice = "Hit"
+            case 11:
+                self.advice = "Hit"
+            case 10:
+                self.advice = "Hit"
+            case 9:
+                self.advice = "Hit"
+            case 8:
+                self.advice = "Hit"
+            case 7:
+                self.advice = "Hit"
+            case 6:
+                self.advice = "Hit"
+            case 5:
+                self.advice = "Hit"
+            case 4:
+                self.advice = "Hit"
+            case _:
+                self.advice = "Bust!"
 
         print(f"{self.total} {self.advice}")
 
@@ -90,7 +114,7 @@ def main() -> None:
     game = Blackjacked(card_1, card_2, card_3)
 
     game.grade_cards()
-    game.advice()
+    game.adviced()
 
 
 if __name__ == "__main__":
