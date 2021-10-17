5271d863952485441fa3562a414b6b0df24f47de
Rafael <realworldsurf@gmail.com>
2021-10-16 22:20:36 -0700

    Lab_06 in progress

---
+++ Code/Rafael/Python/Lab_07.py
@@ -0,0 +1,20 @@
+
+# Lab_06 Peaks and Valleys
+
+"""
+# peaks() Has a lower number on left and right
+# valleys() Has a higher number on left and right
+# peaks_and_valleys() Both the lowest and highest on left and right
+"""
+
+# Lab_06 Peaks and valleys
+# Version 2
+
+# Note: in progress 10/16/21
+
+
+data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+
+for num in data:
+    print('x' * num)
+
