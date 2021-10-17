62d1a8497a366c099ec012022e49bd677f4c6ce8
Christerpher Hunter <djhunter67@gmail.com>
2021-10-17 19:05:56 -0400

    trying to fix git errors

--- Code/ChristerpherHunter/Python/dad_joke.py
+++ Code/ChristerpherHunter/Python/dad_joke.py
@@ -2,6 +2,7 @@
 # PDX Codeguild Lab 10: Dad Joke
 
 from requests import get
+from random import randint
 
 
 class Dad_Joke:
@@ -9,18 +10,43 @@ class Dad_Joke:
 
     def __init__(self) -> None:
 
-        self.dad_joke = dict()
+        self.data = dict()
+        self.search_data = dict()
+        self.url = "https://icanhazdadjoke.com"
+        self.header = {"accept": "application/json"}
 
     def get_joke(self):
         """Use request library to utilize an API"""
 
-        self.dad_joke = get("https://icanhazdadjoke.com/application.json")
+        self.data = get(self.url, headers=self.header).json()
 
-        return self.dad_joke
+        return self.data
+
+    def search_joke(self, search_term):
+        """Search the Dad Joke API for jokes"""
+
+        search_results = list()
+        self.search_data = get(f"{self.url}/search?term={search_term}",
+                               headers=self.header).json()
+
+        search_results = self.search_data['results'][randint(1, "total jokes")]['joke']
+
+        return search_results
 
 
 def main() -> None:
     """Execution and testing"""
 
-    dad_bod = Dad_Joke()
-    print(dad_bod.get_joke)
+    dad_joke = Dad_Joke()
+
+    data = dad_joke.get_joke()
+
+    search_data = dad_joke.search_joke("bee")
+
+    print(f"\n{data.get('joke')}\n")
+
+    print(f"\n{search_data}\n")
+
+
+if __name__ == "__main__":
+    main()
