678c7bc79a82670309c167b358b1048efac4ce38
TheAncientBooer <randall.mayton@gmail.com>
2021-10-18 16:52:44 -0700

    lab06

---
+++ Code/Randall/Python/lab06.py
@@ -0,0 +1,15 @@
+#Lab 6: Credit Card Validation
+
+
+
+cc_num = (input("Enter credit card number: "))
+cc_num = (str(cc_num[:-1]))
+cc_num = cc_num[::-1]
+cc_num = (cc_num[:])
+
+
+
+
+
+print(cc_num)
+    
