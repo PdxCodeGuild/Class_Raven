b3ecea74d19a96781cf190073cac20a1c002767b
Austen Myers <austenc.id@pm.me>
2021-10-18 18:18:48 -0600

    renamed some files

---
+++ Code/Austen/week-2/labs/Oct_19.py
@@ -0,0 +1,6 @@
+def lab_title_1():
+  ''
+
+
+def lab_title_2():
+  ''
---
+++ Code/Austen/week-2/week-2.py
@@ -0,0 +1,29 @@
+def monday():
+  from labs import Oct_18
+
+
+def tuesday():
+  from labs import Oct_19
+
+
+def wednesday():
+  from labs import Oct_20
+
+
+def thursday():
+  from labs import Oct_21
+
+
+def friday():
+  from labs import Oct_22
+
+
+def week_2():
+  monday()
+  tuesday()
+  wednesday()
+  thursday()
+  friday()
+
+
+week_2()
