3ab3d3e922818bd01d2c25519b7c644a6e52adee
Christerpher Hunter <djhunter67@gmail.com>
2021-10-19 00:32:48 -0400

    progress toward non-crash output

--- Code/ChristerpherHunter/Python/rot13_ciper.py
+++ Code/ChristerpherHunter/Python/rot13_ciper.py
@@ -9,36 +9,54 @@ encryption is the same as decryption.
 """
 
 
-class ROT13:
+class ROTN:
     """Encode a string with a Rotation Cipher"""
 
     def __init__(self, message: str, rotation_num=13) -> None:
 
-        self.message = message.lower()
+        self.message = message.lower().replace(" ", "")
         self.rotation_num = rotation_num
         self.encrypted_message = str()
+        self.decrypted_message = str()
+        global ALPHABET
+        ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                    'w', 'x', 'y', 'z']
+
+    def __str__(self) -> str:
+        return self.encrypted_message
 
     def encrypt(self) -> None:
         """Encrypt the message based on the ROT number"""
 
-        self.encrypted_message = [letter for letter in
-                                  map(ROT13.letters(letter, self.rotation),
-                                      self.message)]  # Finish this up
+        self.encrypted_message = map(ROTN.letters, self.message)
 
     def decrypt(self) -> None:
         """Decrypt the message based on the ROT number"""
 
-    def letters(letter: str, rotation: int) -> str:
+        self.decrypted_message = "".join(map(ROTN.letters,
+                                         (self.message, -self.rotation_num)))
+
+    def letters(letter: str, rotation=13) -> str:
         """Takes in a letter and rotation val and returns rotated value"""
 
-        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-                    'y', 'z']
-
-        letter_val = int([num for num, letters in enumerate(alphabet, start=1)
-                          if letter == letters])
+        letter_val = [num for num, letters in enumerate(ALPHABET, start=1)
+                      if letter == letters].pop()
         letter_val += rotation
-        letter_val = str([letters for num, letters in enumerate(alphabet,
-                          start=1) if num == letter_val])
+        letter_val = [letters for num, letters in enumerate(ALPHABET,
+                      start=1) if num == letter_val]
 
         return letter_val
+
+
+def main() -> None:
+
+    message = "hello world"
+    secret = ROTN(message, 13)
+    secret.encrypt()
+    for i in secret.encrypted_message:
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
