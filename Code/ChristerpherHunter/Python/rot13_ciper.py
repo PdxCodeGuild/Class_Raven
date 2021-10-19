3f605462d204104208a33352679e57e5084ff824
Christerpher Hunter <djhunter67@gmail.com>
2021-10-18 20:05:18 -0400

    starting ROT cipher from arch linux

---
+++ Code/ChristerpherHunter/Python/rot13_ciper.py
@@ -0,0 +1,16 @@
+"""
+Author: Christerpher Hunter
+PDX Code guild: Lab 11 ROT13 Cipher
+
+Write a program that prompts the user for a string, and encodes it with ROT13.
+For each character, find the corresponding character, add it to an output
+string. Notice that there are 26 letters in the English language, so
+encryption is the same as decryption.
+"""
+
+
+class ROT13:
+    """Encode a string with a Rotation Cipher"""
+
+    def __init__(self) -> None:
+        pass
