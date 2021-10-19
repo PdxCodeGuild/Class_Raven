5a2e1a689b269cc11df4ffac7a5ab27ff3592b2a
Lee Colburn <colburn.lee@gmail.com>
2021-10-18 17:17:54 -0700

    complete with lab 7

--- Code/Lee/Python/lab_07.py
+++ Code/Lee/Python/lab_07.py
@@ -94,11 +94,10 @@ def peaks_and_valleys(data, pv_final):
 def graph_simple(data):
   """Returns a graph of the dataset in vanilla python"""
   # data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+  # Note - this is really hard. I used numpy and matplotlib to create a chart because I got bogged down :( ...
   y_data = [8, 7, 6, 5, 4, 3, 2, 3, 4, 5, 4, 3, 2, 1, 0, 1, 2, 3, 2, 1, 0]
   for num in zip(data,y_data):
     print('x' * num)
-    spacer= (9-num)
-    print('' * spacer)
   return
 
 
