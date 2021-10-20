7da99d191cea20ff17cdddc21f37509fb9831c75
jhell85 <jhell85@gmail.com>
2021-10-20 13:36:30 -0700

    v3 done with minor bug still working on

--- Code/Josh/lab_7_peaks_and_valleys.py
+++ Code/Josh/lab_7_peaks_and_valleys.py
@@ -28,12 +28,44 @@ def print_land(data):
   while height > 0:
     display_array = []
     for i in range(0,len(data)):
-      if data[i] <= height:
-        display_array.append(' ')
-      else:
+      if data[i] >= height:
         display_array.append('X')
+      else:
+        display_array.append(' ')
     height = height - 1
     print_str =  ' '.join([elem for elem in display_array])
     print(print_str)
     
-print_land(data_list)
+# print_land(data_list)
+
+# V3
+def print_land_water(data):
+  peaks = peaks_func(data)
+  height = 0
+  for i in range(0,len(data)):
+    if data[i] > height:
+      height = data[i]
+  while height > 0:
+    display_array = []
+    for i in range(0,len(data)):
+      skip = False
+      #check if in between two peaks 
+      for p in range(0,len(peaks)):
+        if i <= peaks[p]: #if the index is less then or equal to the first peak break no water would go there
+          break
+        lowest_peak = ''
+        try : # find the lowest peak in the peaks array
+          lowest_peak = data[peaks[p]] if data[peaks[p]] <= data[peaks[p+1]] else data[peaks[p+1]]
+        except IndexError: # if index of peak array out of range use the last element of the data list to compare with
+          lowest_peak = data[peaks[p]] if data[peaks[p]] <= data[-1] else data[-1]
+        if data[i] <= lowest_peak and height <= lowest_peak and height > data[i] :
+          display_array.append('0')
+          skip = True
+      if data[i] >= height:
+        display_array.append('X')
+      elif not skip:
+        display_array.append(' ')
+    height = height - 1
+    print_str =  ' '.join([elem for elem in display_array])
+    print(print_str)
+print_land_water(data_list)
