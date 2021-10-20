dc1384c9253d692484fbd46594938d9fbc3c193c
jhell85 <jhell85@gmail.com>
2021-10-19 22:55:06 -0700

    v2

--- Code/Josh/lab_7_peaks_and_valleys.py
+++ Code/Josh/lab_7_peaks_and_valleys.py
@@ -15,9 +15,25 @@ def valleys_func(data):
   return(valley_array)
   
 
-peaks = peaks_func(data_list)
-valleys = valleys_func(data_list)
-print(f'peaks {peaks}, valleys {valleys}')
+# peaks = peaks_func(data_list)
+# valleys = valleys_func(data_list)
+# print(f'peaks {peaks}, valleys {valleys}')
 
 # V2
-
+def print_land(data):
+  height = 0
+  for i in range(0,len(data)):
+    if data[i] > height:
+      height = data[i]
+  while height > 0:
+    display_array = []
+    for i in range(0,len(data)):
+      if data[i] <= height:
+        display_array.append(' ')
+      else:
+        display_array.append('X')
+    height = height - 1
+    print_str =  ' '.join([elem for elem in display_array])
+    print(print_str)
+    
+print_land(data_list)
