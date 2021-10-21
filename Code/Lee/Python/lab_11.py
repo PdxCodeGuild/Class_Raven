3a08253a87788fdeea40a23f5ba36c377a0e382b
Lee Colburn <colburn.lee@gmail.com>
2021-10-21 08:12:03 -1000

    improved lab11

--- Code/Lee/Python/lab_11.py
+++ Code/Lee/Python/lab_11.py
@@ -3,64 +3,36 @@ Lee Colburn
 Evening Bootcamp - PDX Code Guild
 Lab 11 - Rot Cipher
 """
-
-"""Rot Cipher
-Write a program that prompts the user for a string, and encodes it with ROT13. 
-For each character, find the corresponding character, add it to an output string. 
-Notice that there are 26 letters in the English language, so encryption is the same as decryption.
-
-Index	0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18	19	20	21	22	23	24	25
-English	a	b	c	d	e	f	g	h	i	j	k	l	m	n	o	p	q	r	s	t	u	v	w	x	y	z
-ROT+13	n	o	p	q	r	s	t	u	v	w	x	y	z	a	b	c	d	e	f	g	h	i	j	k	l	m
-
-
-Version 2
-Allow the user to input the amount of rotation used in the encryption. (ROTN)
-
-
-Version 3 (optional)
-Add support for capital letters, numbers, and special characters. These can be handled in two different ways:
-
-Capital letters can be rotated as well, numbers and special characters can be put directly into the output (e.g. "hello world!" becomes "uryyb jbeyq!").
-
-Instead of using an alphabet of just letters, include numbers, spaces, and special characters as well.
-"""
 import string
 
 def encrypt(user_string,rot):
-    alphabet_lowercase = string.ascii_lowercase
-    alphabet_uppercase = string.ascii_uppercase
-    shifted_alphabet_lowercase = alphabet_lowercase[26-rot:] + alphabet_lowercase[0:(26-rot)] 
-    shifted_alphabet_uppercase = alphabet_uppercase[26-rot:] + alphabet_uppercase[0:(26-rot)]
+    """Takes in a string and integer to set a rotated alphabet. Encrypts a messsage and returns the string"""
+    alphabet_printable = string.printable + " " # 101 chars: " " 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ 
+    shifted_alphabet_printable = alphabet_printable[101-rot:] + alphabet_printable[0:(101-rot)] # rotates string to correspond with printable alphabet
     encrypted_message = ""
 
-
     for i in range(len(user_string)):
-        if alphabet_lowercase.find(user_string[i]) == -1:
+        encrypted_index = alphabet_printable.find(user_string[i])
+        if encrypted_index == -1:
             encrypted_message = encrypted_message + user_string[i]
-        elif user_string[i].isupper() == True:
-            encrypted_index = alphabet_uppercase.find(user_string[i])
-            encrypted_message = encrypted_message + shifted_alphabet_uppercase[encrypted_index]
-        else: #user_string[i].islower() == True:
-            encrypted_index = alphabet_lowercase.find(user_string[i])
-            encrypted_message = encrypted_message + shifted_alphabet_lowercase[encrypted_index]
+        else:
+            encrypted_message = encrypted_message + shifted_alphabet_printable[encrypted_index]
         
-    print(encrypted_message)
     return encrypted_message
 
 def decrypt(user_string, rot):
     """Returns a decrypted string based on the message and data offset"""
-    decrypted_string = encrypt(encrypt(user_string,rot),26-rot)
+    decrypted_string = encrypt(user_string,101-rot)
+    print(f"Your decrypted string is: {decrypted_string}\n Returning to main menu")
     return decrypted_string
 
 ####################### INTERFACE ##############################################################
 complete = False
 
 while not complete:
-
   # Select Option 1-Pass Message, 2-Exit, 3+ Try Again
   start = int(input(f'\nPlease select from the following options:\n 1. Encode a string with ROT based encryption \n 2. Decrypt last message (or your own) \n 3. Exit Program \n\n Enter the number of your choice: \n'))
-  
+
   ################### DIRECTORY ################################################################
   # Allow user to escape
   if start == 3:
@@ -76,20 +48,21 @@ while not complete:
   if start == 1:
 
     user_string = input("Enter your string: ")
-    rot = int(input("Enter the number of digits you'd like to offset the data (1-26): "))
-    encrypted_string = encrypt(user_string.lower(), rot)
+    rot = int(input("Enter the number of digits you'd like to offset the data (1-101): "))
+    encrypted_string = encrypt(user_string, rot) # lower only test: encrypted_string = encrypt(user_string.lower(), rot)
+    print(f"\nYour encrypted string is: {encrypted_string}\n\n If you'd like to decrypt this string, please remember it has been offset {rot} digits. \n Returning to main menu...")
 
   if start == 2:
-    decrypted_string = " "
-    selection = input(f"Would you like to: \n 1. If available, decrypt the stored message: {encrypted_string} \n 2. Decrypt your own message ")
+    decrypted_string = ""
+    selection = int(input(f"Would you like to: \n 1. If available, decrypt the stored message: {encrypted_string} \n 2. Decrypt your own message \n"))
+
     if selection == 1:
-        rot = int(input("Enter the number of digits you'd like to offset the data for decryption (1-26): "))
+        rot = int(input("Enter the amount of digits the string is offset (1-101): \n"))
         decrypt(encrypted_string, rot)
         print(decrypted_string)
 
     if selection == 2:
-        rot = int(input("Enter the number of digits you'd like to offset the data for decryption(1-26): "))
-        user_string = input("Enter your string: ")
-        decrypt(user_string, rot)
-        print(decrypted_string)
-
+        user_string = input("Enter the encrypted string: ")
+        rot = int(input("Enter the amount of digits the string is offset (1-101): \n"))
+        decrypted_string = decrypt(user_string, rot)
+        print(f"Decrypted string: {decrypted_string} \n")
