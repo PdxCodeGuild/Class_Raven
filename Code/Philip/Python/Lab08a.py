64a9a3cee0d8eb0dca8376e6cd3d70530fb15374
Philip Bartoo <Philip_99bartoo@hotmail.com>
2021-10-20 18:23:49 -0700

    Completed

--- Code/Philip/Python/Lab08a.py
+++ Code/Philip/Python/Lab08a.py
@@ -1,3 +1,7 @@
+'''Pick6 Lab08
+By Philip Bartoo
+PDX Code Guild'''
+
 from random import randint
 #Request the number of player tickets
 tickets = int(input('Enter the number of tickets: '))
@@ -18,33 +22,41 @@ print(f'Winning ticket: {winning_ticket}')
 
 #Create the logic to loop through and generate player tickets
 total_winnings = 0
+winnings = 0
 i=0
 while i < tickets:
     player_ticket = generate_ticket()
     #print(f'Player ticket: {player_ticket}')
     i=i+1
+    index = 0
     #Evaluate player tickets to determine winnings
-    winnings = 0
-    if player_ticket[:6] == winning_ticket[:6]:
-        winnings = 25000000
-    elif player_ticket[:5] == winning_ticket[:5]:
-        winnings = 1000000
-    elif player_ticket[:4] == winning_ticket[:4]:
-        winnings = 5000
-    elif player_ticket[:3] == winning_ticket[:3]:
-        winnings = 100
-    elif player_ticket[:2] == winning_ticket[:2]:
-        winnings = 7
-    elif player_ticket[:1]== winning_ticket[:1]:
-        winnings = 4
-    #Track total winnings for the loop
-    total_winnings = total_winnings + winnings
+    match = 0
+    while index < len(player_ticket):
+        if player_ticket[index] == winning_ticket[index]:
+            match += 1
+        index = index + 1
+    #winnings = 0
+    if match == 6:
+        winnings += 25000000
+    elif match == 5:
+        winnings += 1000000
+    elif match == 4:
+        winnings += 5000
+    elif match == 3:
+        winnings += 100
+    elif match == 2:
+        winnings += 7
+    elif match == 1:
+        winnings += 4
+#Track total winnings for the loop
+#total_winnings = total_winnings + winnings
 #Build in ticket cost
 cost = tickets * 2
 #Determine final balance
-balance = total_winnings - cost
+balance = winnings - cost
 #Calculate return on investment
-roi = (total_winnings-cost)/cost
+roi = (winnings-cost)/cost
 #Print the balance and roi
-print(balance)
+print(f'${balance}')
+print(f'${winnings}')
 print(roi)
