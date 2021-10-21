35b93cc24b2923a3b628a7e019168ee58b188627
SGTWOBBLE <aaron.quiroz23@yahoo.com>
2021-10-20 17:28:16 -1000

    lab 5 and lab 8 working on

--- Code/Aaron/Python/lab5.py
+++ Code/Aaron/Python/lab5.py
@@ -1,14 +1,34 @@
-def palindrome(string):
-    if(len(string) == 0):
-        return string
-    else:
-        return palindrome(string[1:]) + string[0]
+# def palindrome(string):
+#     if(len(string) == 0):
+#         return string
+#     else:
+#         return palindrome(string[1:]) + string[0]
 
-string1 = input("enter a word: ")
-string = palindrome(string1)
-# print(string1 + "is not a plalindrome")
+# string1 = input("enter a word: ")
+# string = palindrome(string1)
+# # print(string1 + "is not a plalindrome")
 
-if(string1 == string):
-    print(string1 + " is a palindrome")
+# if(string1 == string):
+#     print(string1 + " is a palindrome")
+# else:
+#     print(string1 + " is not a palindrome")
+
+
+#I have been trying to make all this work together and I just looked at the solotion and its separate functions
+
+#I wanted to see how the program worked so I used the solution from the github. im currently going work on the 
+
+def check_anagram(word):
+    word = word.lower()
+    word = word.replace(' ', '')
+    word = str(sorted(word))
+    return word
+
+anagram_ = input('what the 1st word?: ')
+anagram1 = input('what is the 2nd word?: ') 
+print('anagram_: '+anagram_)
+print('anagram1: '+anagram1)
+if check_anagram(anagram_) == check_anagram(anagram1):
+    print(anagram_ + ' and ' + anagram1 + ' is a anagram' )
 else:
-    print(string1 + " is not a palindrome")
+    print(anagram_  + ' and ' +  anagram1 +  ' are not a anagram')
---
+++ Code/Aaron/Python/lab6.py
@@ -0,0 +1,81 @@
+# Lab 06... again: Credit Card Validation
+from colorama import Fore as F
+R = F.RESET
+class Validation:
+    def __init__(self, credit_card_nums: str) -> None:
+        # Security in mind for initialization variables
+        self.valid_cc = bool()
+        self.__init_check_digit = int()  # Private variable for the check digit
+        self.__pulled_check_digit = int()  # The final digit to check against
+        self.__credit_card_nums_str = credit_card_nums  # The CC numbers str
+        self.__credit_card_nums = list()  # The credit card nums as an int list
+    # Take in the CC numbers as a string and return list of ints
+    def str_to_int(self) -> list:
+        # Remove all spaces in entry
+        temp = self.__credit_card_nums_str.replace(" ", "")
+        # Make list of ints
+        for i in temp:
+            self.__credit_card_nums.append(int(i))
+    # Conserve the check digit
+    def check_digit(self) -> int:
+        self.__pulled_check_digit = self.__credit_card_nums[-1]
+        # Remove the last index of the list
+        self.__credit_card_nums.pop(-1)
+    # Reverse the digits
+    def rev_digits(self) -> list:
+        self.__credit_card_nums.reverse()
+    # Double every other element in the reversed list
+    def double_the_odds(self) -> list:
+        temp = []
+        for element in range(len(self.__credit_card_nums)):
+            if element % 2 != 0:
+                temp.append(self.__credit_card_nums[element])
+            else:
+                temp.append(self.__credit_card_nums[element] * 2)
+        self.__credit_card_nums = temp.copy()
+    # Subtract nine from elements over 9
+    def take_nines(self) -> list:
+        temp = list()
+        for element in self.__credit_card_nums:
+            if element > 9:
+                temp.append(element - 9)
+            else:
+                temp.append(element)
+        self.__credit_card_nums = temp[:]
+    # Sum all elements of the list
+    def summer(self) -> int:
+        for element in self.__credit_card_nums:
+            self.__init_check_digit += element
+    # Extract the second digit of the integer
+    def extraction(self) -> int:
+        # Modulate
+        self.__init_check_digit = self.__init_check_digit % 10
+    # Checker
+    def checker(self) -> bool:
+        self.valid_cc = self.__pulled_check_digit == self.__init_check_digit
+        return self.valid_cc
+    def __str__(self) -> str:
+        return str(self.valid_cc)
+def main() -> None:
+    # Validation and testing
+    # Take in cc numbers
+    cc_nums = input("\nPlease enter the full CC number: ")
+    real_cc = Validation(cc_nums)
+    real_cc.str_to_int()
+    # Take the last digit
+    real_cc.check_digit()
+    # Reverse the digits
+    real_cc.rev_digits()
+    # Double every other element
+    real_cc.double_the_odds()
+    # Subtract nines from numbers over nine
+    real_cc.take_nines()
+    # Sum all values
+    real_cc.summer()
+    # Take the ones digit
+    real_cc.extraction()
+    # Check for match
+    print(f"\nThe credit card enter is valid:\
+ {F.YELLOW}{real_cc.checker()}{R}")
+if __name__ == '__main__':
+    main()
