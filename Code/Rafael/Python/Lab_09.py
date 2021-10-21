6dd11af1ae10c94ae77ceb4e3ffd73228810a559
Rafael <realworldsurf@gmail.com>
2021-10-20 23:39:11 -0700

    Lab_09 version 1

--- Code/Rafael/Python/Lab_09.py
+++ Code/Rafael/Python/Lab_09.py
@@ -1,6 +1,6 @@
 # Lab_09 Blackjack Advice
 
-
+# dic for card values
 card_values = {
 'a': 1,
 '2': 2,
@@ -17,21 +17,24 @@ card_values = {
 'k': 13
 }
 
+# input converted to a string
 first_card = input(str('"j" for Jack, "q" for Queen, "k" for king, "a" for Ace lower-case only or "1-10"\nWhat\'s your first card:? '))
 
 second_card = input(str('"j" for Jack, "q" for Queen, "k" for king, "a" for Ace lower-case only or "1-10"\nWhat\'s your second card:? '))
 
 third_card = input(str('"j" for Jack, "q" for Queen, "k" for king, "a" for Ace lower-case only or "1-10"\nWhat\'s your third card:? '))
 
-
+# inputs for draw take the key and assigns an int type 
 first_draw = card_values[first_card]
 
 second_draw = card_values[second_card]
 
 third_draw = card_values[third_card]
 
-
+# total of 3 draws
 total_draw = first_draw + second_draw + third_draw
+
+# while loop breaking if boolean is True
 while True:
     if total_draw < 17:
         print(f'{total_draw} "Hit"')
@@ -41,7 +44,7 @@ while True:
         break
     if total_draw == 21:
         print(f'{total_draw} "Blackjack!"')
-    if total_draw > 21:
+    elif total_draw > 21:
         print(f'{total_draw} "Already Busted"')
         break
     
