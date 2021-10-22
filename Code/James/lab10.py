f86dbba2aaa7e2d5126063c6ddfec6189ef870b5
James Johnson <jamesjhnsn1995@gmail.com>
2021-10-22 15:45:29 -0700

    lab10 v3 incomplete

--- Code/James/lab10.py
+++ Code/James/lab10.py
@@ -1,13 +1,24 @@
 import requests
 #user_search = input("Enter a search term: ")
 
-response = requests.get('https://icanhazdadjoke.com'
+response = requests.get('https://icanhazdadjoke.com/search'
 ,headers = {
 'Accept': 'application/json'
 })
 
 
-#how to take user input and search the website with that term.
-data = response.json()
-print(data)
+#how to take user input and search the website with that
+data = response.json() # this is saved as dictionary
 
+print(type(data))
+
+
+#how to search through all the dictionaries to find the users search term?
+
+
+user = input('Enter a search term: ')
+
+if user in data.values():
+    print(data.values())
+# find = data[user]
+# print(find)
