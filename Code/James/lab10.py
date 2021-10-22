b0ed2cc775c98b5f4ec0eb6c13488f43e0191362
James Johnson <jamesjhnsn1995@gmail.com>
2021-10-22 15:20:33 -0700

    lab10 complete and proper lab#

--- Code/James/lab10.py
+++ Code/James/lab10.py
@@ -1,50 +1,13 @@
-""" rot cipher"""
-import string
+import requests
+#user_search = input("Enter a search term: ")
+
+response = requests.get('https://icanhazdadjoke.com'
+,headers = {
+'Accept': 'application/json'
+})
 
 
+#how to take user input and search the website with that term.
+data = response.json()
+print(data)
 
-#generating two lists and then fusing them in a dictionary
-alpha = string.ascii_lowercase
-# num = [i for i in range(26)]
-# res = {alpha[i]: num[i] for i in range(len(alpha))}
-
-# taking user input
-while True:
-    user = input("Enter your message here: \n>>>").lower()
-    if not user.isalpha():
-        print("Please enter only letters.")
-        continue
-    
-    rot = input("Enter your rotation: ")
-    if not rot.isnumeric():
-        print("Please enter only numbers.")
-        continue
-    else:
-        rot = int(rot)
-
-    user_output = []
-    #for each character in user input add it to an output list
-    for element in user:
-        #alpha.append(element)
-        index = alpha.find(element)
-        encrypt = (index + rot) % 26
-        print(encrypt)
-        user_output.append(alpha[encrypt])
-        print(user_output)
-    # for each element in the list match it the dictionary index
-
-    print(''.join(user_output))
-    break
-# new_index = index + 13
-
-# a
-
-
-# for element in user_output:
-#     if element in res:
-#         element + 13
-        #move the element 13 positions to the right
-
-
-
-#print(user_output)
