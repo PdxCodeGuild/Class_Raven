275d49ad9634837ff2a3bbd8d09865d5bc99c72b
Christerpher Hunter <djhunter67@gmail.com>
2021-10-23 17:11:53 -0400

    accomplished the initial task

--- Code/ChristerpherHunter/Python/count_words.py
+++ Code/ChristerpherHunter/Python/count_words.py
@@ -21,6 +21,10 @@ class Count:
     def __init__(self) -> None:
 
         self.texts = None
+        self.lists = list()
+        self.count = int()
+        self.dict_count = dict()
+        self.top_ten = list()
 
     def get_text(self) -> None:
         """Retrieve the text"""
@@ -32,14 +36,66 @@ class Count:
         """Create the text file"""
 
         with open("book_text.txt", 'w', encoding="utf-8") as file:
-            file.write(self.texts)
+            file.write(self.texts.text)
+
+    def open_text(self) -> str:
+        """Open the text and work with it"""
+
+        with open("book_text.txt", "r") as r_file:
+            self.texts = r_file.read()
+
+        return self.texts
+
+    def make_list(self) -> list:
+        """Make everything lowercase, strip punctuation, and split to a list"""
+
+        self.lists = [self.texts
+                      .split()[i]
+                      .replace(";", "")
+                      .replace(",", "")
+                      .replace("'", "")
+                      .replace("?", "")
+                      .replace("!", "")
+                      .replace(".", "")
+                      .replace("*", "")
+                      .replace("(", "")
+                      .replace(")", "")
+                      .replace("/", "")
+                      .replace('"', "")
+                      .replace("_", "")
+                      .lower() for i in range((len(self.texts) // 1_000))]
+
+        return self.lists
+
+    def make_dict(self) -> dict:
+        """Key is the word and the Value is the occurence"""
+
+        for i in self.lists:
+            self.dict_count[i] = self.lists.count(i)
+
+        return self.dict_count
+
+    def highest_count(self) -> list:
+        """Return the top 10 highest count words"""
+
+        intermediate_list = list(self.dict_count.items())
+        intermediate_list.sort(key=lambda tup: tup[1], reverse=True)
+        for i in range(min(10, len(intermediate_list))):
+            self.top_ten.append(intermediate_list[i])
+
+        return self.top_ten
 
 
 def main() -> None:
 
     words = Count()
-    words.get_text()
-    words.create_text()
+    # words.get_text()
+    # words.create_text()
+    words.open_text()
+    words.make_list()
+    words.make_dict()
+    high_count = words.highest_count()
+    print(high_count)
 
 
 if __name__ == "__main__":
