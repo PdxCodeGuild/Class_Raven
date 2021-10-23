e3942f45900be0432c93fe4c366180a1ec02e992
Michael <broetjem@gmail.com>
2021-10-22 20:20:30 -0700

    9,11,13 complete, formatting changes in rest

--- Code/Michael/Python/10-dad_joke_api.py
+++ Code/Michael/Python/10-dad_joke_api.py
@@ -1,7 +1,7 @@
-'''
+"""
 PDX Code Guild Full Stack Bootcamp
-->Lab 08
-  Pick6
+->Lab 10
+  Dad Jokes API
 Michael B
 
 Dad Joke API
@@ -14,54 +14,58 @@ Part 2 (optional)
 Add the ability to "search" for jokes using another endpoint. Create a REPL that allows one to enter a search term and go through jokes one at a time. You can also add support for multiple pages.
 
 requests.get(url, {'accept': 'application/json'}) 
-'''
+"""
 
 
-def track_keys():
+def track_keys() -> None:
     from pynput.keyboard import Key, Listener
+
     def on_press(key):
-        print('{0} pressed'.format(
-            key))
+        print("{0} pressed".format(key))
         if key == Key.esc:
             return False
 
     with Listener(
-            on_press=on_press,
-            ) as listener:
+        on_press=on_press,
+    ) as listener:
         listener.join()
 
-def main_program():
+
+def main_program() -> None:
     from time import sleep
     import requests
-    
-    extra_punct = "" # Sometimes puncutation is cut.
-    response = requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'application/json'})
+
+    extra_punct = ""  # Sometimes puncutation is cut.
+    response = requests.get(
+        "https://icanhazdadjoke.com/", headers={"Accept": "application/json"}
+    )
     json_joke = response.json()
-    asked_joke = json_joke['joke'].split('?',1)
+    asked_joke = json_joke["joke"].split("?", 1)
     punctuation = "?"
-    if len(asked_joke)==1: # If not ? (that didn't split).
-        asked_joke = json_joke['joke'].split(',',1)
+    if len(asked_joke) == 1:  # If not ? (that didn't split).
+        asked_joke = json_joke["joke"].split(",", 1)
         punctuation = ","
-    if len(asked_joke)==1: # if not ? and , (they didn't split).
-        asked_joke = json_joke['joke'].split('.',1)
+    if len(asked_joke) == 1:  # if not ? and , (they didn't split).
+        asked_joke = json_joke["joke"].split(".", 1)
         punctuation = "."
-    print(f'{asked_joke[0].strip()}{punctuation}') # Readd puncuation that was split.
-    for i in range(1,4): # comedic suspense
+    print(f"{asked_joke[0].strip()}{punctuation}")  # Readd puncuation that was split.
+    for i in range(1, 4):  # comedic suspense
         sleep(0.35)
         print("." * i)
-    if asked_joke[1] != "": # Joke is not a one liner.
-        print(f'{asked_joke[1].strip()}{extra_punct}')
-    sleep(3) # Flair.
-    print(f'\n\nHahahahah')
+    if asked_joke[1] != "":  # Joke is not a one liner.
+        print(f"{asked_joke[1].strip()}{extra_punct}")
+    sleep(3)  # Flair.
+    print(f"\n\nHahahahah")
     sleep(0.25)
     print("ha")
 
-from multiprocessing import Process
 
-if __name__ == '__main__':
-    p1 = Process(target=track_keys)
-    p1.start()
+if __name__ == "__main__":
+    from multiprocessing import Process
+
+    # p1 = Process(target=track_keys)
+    # p1.start()
     p2 = Process(target=main_program)
     p2.start()
-    p1.join()
+    # p1.join()
     p2.join()
