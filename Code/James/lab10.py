45f183440be6fed573bd1436afe73dcaec84721f
James Johnson <jamesjhnsn1995@gmail.com>
2021-10-22 20:35:30 -0700

    lab10 v3 complete

--- Code/James/lab10.py
+++ Code/James/lab10.py
@@ -1,24 +1,26 @@
 import requests
 #user_search = input("Enter a search term: ")
 
-response = requests.get('https://icanhazdadjoke.com/search'
+user = input('Enter a search term: ')
+
+response = requests.get(f'https://icanhazdadjoke.com/search?term={user}'
 ,headers = {
 'Accept': 'application/json'
-}, search = {'search_term': ""}) # trying to add a search term in the api
+}) # trying to add a search term in the api
 
 
 #how to take user input and search the website with that
 data = response.json() # this is saved as dictionary
 
-print(data)
+print(data['results'][0]['joke'])
 
 
 #how to search through all the dictionaries to find the users search term?
 
 
-user = input('Enter a search term: ')
 
-if user in data.values():
-    print(data.values())
+
+# if user in data.values():
+#     print(data.values())
 # find = data[user]
 # print(find)
