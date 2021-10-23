d696d74036d828a9fa73f51088f53298488e0308
James Johnson <jamesjhnsn1995@gmail.com>
2021-10-22 19:13:17 -0700

    version 3 changes

--- Code/James/lab10.py
+++ Code/James/lab10.py
@@ -4,13 +4,13 @@ import requests
 response = requests.get('https://icanhazdadjoke.com/search'
 ,headers = {
 'Accept': 'application/json'
-})
+}, search = {'search_term': ""}) # trying to add a search term in the api
 
 
 #how to take user input and search the website with that
 data = response.json() # this is saved as dictionary
 
-print(type(data))
+print(data)
 
 
 #how to search through all the dictionaries to find the users search term?
