8561e47acaa19e22fc970b4c1fa6aae67b89a379
James Johnson <jamesjhnsn1995@gmail.com>
2021-10-28 16:47:35 -0700

    lab15 completed

--- Code/James/lab15.py
+++ Code/James/lab15.py
@@ -1,23 +1,78 @@
 """ Linear search """
 
-user_input = int(input('Enter the number you want the index of: '))
 
+nums = [1, 2, 3, 4, 5, 6, 7, 8]
 
-def linear_search(nums, value):
+def linear_search(list, value):
     match = 'No match found'
     for i in range(len(nums)):
         if nums[i] == value:
-            
             match = i 
     print(match)
     return match
-nums = [1, 2, 3, 4, 5, 6, 7, 8]
 
-linear_search(nums, user_input)
+
+#linear_search(nums, 3)
 
 
 """ binary search """
-  
+def binary_search(list, target):
+
+    nums.sort()
+    low = min(nums)
+    #print(low)
+    high = max(nums)
+    #print(high)
+    middle = (low + high) // 2
+    
+    while low <= high:
+        middle = (low + high) // 2
+        
+        if nums[middle] == target:
+            return middle
+        
+        if nums[middle] > target:
+            high = middle - 1
+        
+        else:
+            low = middle + 1
+            
+        
+        
+        
+
+#print(binary_search(nums, 3))
+
+
+
+
+
+
+""" bubble sort """
+
+bubble_nums = [64, 34, 25, 12, 22, 11, 90]
+
+
+def bubble_sort(array):
+    
+    swapped = True
+
+    while(swapped):
+        swapped = False
+        for i in range(len(array) - 1): # will run until second to last item in list
+            if array[i] > array[i + 1]:
+                # Swap the item in the list if it's greater then it's neighbor.
+                array[i], array[i + 1] = array[i + 1], array[i]
+                swapped = True
+    return array
+
+
+print(bubble_sort(bubble_nums))
+
+    
+    
+
+
 
 
 
