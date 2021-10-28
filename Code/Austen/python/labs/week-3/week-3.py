01a2edda002d04b2ad12514f07af20a171cc1826
Austen Myers <austenc.id@pm.me>
2021-10-27 23:21:56 -0600

    completed lab_search_sort

--- Code/Austen/python/labs/week-3/week-3.py
+++ Code/Austen/python/labs/week-3/week-3.py
@@ -1,35 +1,59 @@
 class importer:
-    def system():
-        import sys
-        default = 'X:\\pdx-code-guild\\'
-        root = input(
-            f'enter your directory upto Class_Raven: \n (example: {default})\n')
-        if root == 'default':
-            root = default
-        utilspath = f'{root}Class_Raven\\Code\\Austen\\python\\'
-        sys.path.append(utilspath)
+  def directory():
+    import os
+    current_directory = os.getcwd()
+    current_directory = os.path.dirname(current_directory)
+    current_directory = os.path.dirname(current_directory)
+    return current_directory
+
+  def path():
+    import sys
+    directory = importer.directory()
+    path = sys.path.append(directory)
+    return path
+
+  def response():
+    importer.path()
+    from utilities.responses import response
+    return response
+
+  def timestamp():
+    importer.path()
+    from utilities.timestamp import now
+    return now
+
+  def labs():
+    from workbook.lab_ATM import lab_ATM
+    from workbook.lab_search_sort import lab_search_sort
+    labs = [lab_ATM, lab_search_sort]
+    return labs
 
 
 def week_3():
-    importer.system()
-    from utilities.responses import response
-    from utilities.timestamp import now
-    from workbook import Oct_25
-    labs = ['ATM', 'done']
-    lab = input(f'Which lab?\n{labs}\n')
-    lab = response.validate(lab, labs)
+    response = importer.response()
+    now = importer.timestamp()
+    labs = importer.labs()
+    lablist = ['ATM', 'search', 'done']
+    lab = input(f'Which lab?\n{lablist}\n')
+    lab = response.validate(lab, lablist)
     results = []
     while lab != 'done':
-      if lab == 'ATM':
-        result = Oct_25.lab_ATM()
-        stamp = now.result()
-        result = f'{stamp} \n- [{result}]'
-        results.append(result)
-      lab = input(f'Which lab?\n{labs}\n')
-      lab = response.validate(lab, labs)
+        if lab == 'ATM':
+            result = labs[0]()
+            stamp = now.result()
+            result = f'{stamp} \n- [{result}]'
+            results.append(result)
+        elif lab == 'search':
+            result = labs[1]()
+            stamp = now.result()
+            result = f'{stamp} \n- [{result}]'
+            results.append(result)
+        lab = input(f'Which lab?\n{lablist}\n')
+        lab = response.validate(lab, lablist)
     file = open('data/results.log', 'a')
     for result in results:
-      file.write(f'\n{result}')
+        file.write(f'\n{result}')
+        print(result)
 
 
 week_3()
