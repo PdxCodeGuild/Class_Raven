8ce8cfb52c5d8b009ddd0587b7b7e7391913363b
rodney_russell@hotmail.com <rodney_russell@hotmail.com>
2021-10-28 21:43:39 -0400

    notes

--- Code/Rodney/Python/notes.py
+++ Code/Rodney/Python/notes.py
@@ -1,3 +1,22 @@
-card_1 = input("What's your first card?\n> ").upper
 
-print(card_1)
+
+normal_alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+new_alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+used_letters = []
+new_letter = ''
+user_number = 5
+#x = 26 - user_number
+#normal_alphabet2 = normal_alphabet2[:x:]
+
+for letter in normal_alphabet:
+    if letter in used_letters:
+        continue
+    index = normal_alphabet.index(letter) 
+    new_index = index + user_number
+    new_letter = normal_alphabet[new_index]
+    new_alphabet[index] = normal_alphabet[new_index]
+    new_alphabet[new_index] = normal_alphabet[index]
+
+
+
+print(new_alphabet)
