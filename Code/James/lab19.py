ec6e71973cef152740c5d7c1db76d66d971bf7d1
James Johnson <jamesjhnsn1995@gmail.com>
2021-11-01 19:56:08 -0700

    incomplete lab19

--- Code/James/lab19.py
+++ Code/James/lab19.py
@@ -3,17 +3,28 @@ import html
 import requests
 
 class trivia:
-    #def __init__(self):
-    def load():
+    def __init__(self):
+        self.info = []
+
+
+    def load(self):
         response = requests.get('https://opentdb.com/api.php?amount=10&category=23', params={'format': 'json'})
         #print(response.text)
-        data = response.json()
-        print(data)
+        contents = response.json() # this is python dictionary
+        # now get question out of the dictionary
+        questions = contents['results'] # gives all the data from the api
+        
+        self.info = questions # now the class is initialized
+        
+        print(self.info)
+    
+    
+    # def print():
 
-
+trivia = trivia() # create instance of our class
 trivia.load()
 
 
-print('Welcome to history trivia')
-while True:
+# print('Welcome to history trivia')
+# while True:
     
