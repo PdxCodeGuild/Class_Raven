a8fb5d1496347ee2c0ff0020b3dfd66efb122335
James Johnson <jamesjhnsn1995@gmail.com>
2021-11-01 18:37:07 -0700

    lab 17 completed

--- Code/James/lab17.py
+++ Code/James/lab17.py
@@ -22,34 +22,64 @@ class ContactList:
         ...
     
     def count(self):
-        return print(len(self.contacts))
+        return len(self.contacts)
         
         ...
     
     def save(self):
-        new_dict = {'contacts': self.contacts}
-        dict_json = json.dumps(new_dict)
-        print(dict_json)
-        with open(r'C:/Users/james/Class_Raven/Code/James/contacts', 'w') as Contact_list:
-            
-        
+        with open('contacts.json', 'w') as contacts_file:
+            # put self.contacts in a dictionary with the key 'contacts'
+            contacts_dict = {
+                'contacts': self.contacts
+            }
+            # convert the dictionary to a json string (json.dumps)
+            contents = json.dumps(contacts_dict, indent=2)
+            # write the json string to the file
+            contacts_file.write(contents)
         
         
         ...
 
-    # def print(self):
+    def print(self):
+        for contact in self.contacts:
         
-    #     ...
+            display = (
+                    f"Name: {contact['name']}\n"
+                    f"Phone: {contact['phone_number']}\n"
+                    f"Email: {contact['email']}\n"
+                )
+            print(display)
+        
+        ...
 
-    # def add(self, name, phone_number, email):
-       
-    #     ...
-    
-    # def remove(self, name):
+    def add(self, name, phone_number, email):
         
-    #     ...
+        # searches with the input givin in the terminal to append to the contacts list.
+        self.contacts.append({
+            'name': name,
+            'phone_number': phone_number,
+            'email': email
+        })
+       
+        ...
     
-    # def update(self, old_name, new_name, new_phone_number, new_email):
+    def remove(self, name):
+        for index in range(self.count):
+            contact = self.contacts[index]
+            if contact['name'] == name:
+                return self.contacts.pop(index)
+        ...
+    
+    def update(self, old_name, new_name, new_phone_number, new_email):
+        for index in range(self.count):
+            contact = self.contacts[index]
+            if contact['name'] == old_name:
+                self.contacts[index] = {
+                    'name': new_name,
+                    'phone_number': new_phone_number,
+                    'email': new_email
+                }
+
         
         ...
 
@@ -57,45 +87,46 @@ class ContactList:
 
 contact_list = ContactList() # create an instance of our class
 contact_list.load()
-#contact_list.count()
-contact_list.save()
-# print('Welcome to the Contact List App (CLA)')
-# while True:
-#     command = input('Enter a command: ')
-#     if command == 'load':
-#         contact_list.load()
-#         print(f'Loaded ${contact_list.count()} contacts.')
-#     elif command == 'save':
-#         contact_list.save()
-#         print(f'Saved ${contact_list.count()} contacts.')
-#     elif command == 'print':
-#         contact_list.print()
-#     elif command == 'add':
-#         print('Enter info of contact to add:')
-#         name = input('Name: ')
-#         phone_number = input('Phone Number: ')
-#         email = input('Email: ')
-#         contact_list.add(name, phone_number, email)
-#     elif command == 'remove':
-#         name = input('Name of contact to remove: ')
-#         contact_list.remove(name)
-#     elif command == 'update':
-#         print('Enter info of contact to add:')
-#         old_name = input('Name of contact to update: ')
-#         new_name = input('New Name: ')
-#         new_phone_number = input('New Phone Number: ')
-#         new_email = input('New Email: ')
-#         contact_list.update(old_name, new_name, new_phone_number, new_email)
-#     elif command == 'help':
-#         print('Available commands:')
-#         print('load   - load all contacts from the file')
-#         print('save   - save contacts to a file')
-#         print('print  - print all contacts')
-#         print('add    - add a new contact')
-#         print('remove - remove a contact')
-#         print('update - update a contact')
-#         print('exit   - exit the program')
-#     elif command == 'exit':
-#         break
-#     else:
-#         print('Command not recognized')
+
+
+
+print('Welcome to the Contact List App (CLA)')
+while True:
+    command = input('Enter a command: ')
+    if command == 'load':
+        contact_list.load()
+        print(f'Loaded {contact_list.count()} contacts.')
+    elif command == 'save':
+        contact_list.save()
+        print(f'Saved {contact_list.count()} contacts.')
+    elif command == 'print':
+        contact_list.print()
+    elif command == 'add':
+        print('Enter info of contact to add:')
+        name = input('Name: ')
+        phone_number = input('Phone Number: ')
+        email = input('Email: ')
+        contact_list.add(name, phone_number, email)
+    elif command == 'remove':
+        name = input('Name of contact to remove: ')
+        contact_list.remove(name)
+    elif command == 'update':
+        print('Enter info of contact to add:')
+        old_name = input('Name of contact to update: ')
+        new_name = input('New Name: ')
+        new_phone_number = input('New Phone Number: ')
+        new_email = input('New Email: ')
+        contact_list.update(old_name, new_name, new_phone_number, new_email)
+    elif command == 'help':
+        print('Available commands:')
+        print('load   - load all contacts from the file')
+        print('save   - save contacts to a file')
+        print('print  - print all contacts')
+        print('add    - add a new contact')
+        print('remove - remove a contact')
+        print('update - update a contact')
+        print('exit   - exit the program')
+    elif command == 'exit':
+        break
+    else:
+        print('Command not recognized')
