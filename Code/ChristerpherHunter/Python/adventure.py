3fcc0c494c8578807bb66974c46d68da61d6f753
Christerpher Hunter <djhunter67@gmail.com>
2021-11-03 21:27:21 -0400

    init adventure.py

---
+++ Code/ChristerpherHunter/Python/adventure.py
@@ -0,0 +1,18 @@
+"""
+Christerpher Hunter
+Lab 20: Adventure Game
+
+Create a Character and Enemy class.
+
+Create a game board with a list of lists.
+
+Randomly place enemies on the board.
+
+Write a REPL that allows the character to move around
+the board until they encounter an enemy, then battle
+the enemy until one of the characters is defeated.
+
+If the character defeats all the enemies, they win. Otherwise, they lose.
+"""
+
+
