bf4a6d4646078d8dd11c93113f62e926ee9375d2
unknown <keegood8@gmail.com>
2021-11-03 19:01:35 -0700

    updated

--- 1 Python/solutions/lab19_trivia/lab19_trivia.py
+++ 1 Python/solutions/lab19_trivia/lab19_trivia.py
@@ -1,112 +1,81 @@
 import requests
 import html
-from question_options import difficulties, types, categories 
 
 class Trivia:
     def __init__(self):
         self.questions = self.fetch_questions()
-        self.options = {
-            'difficulties': difficulties,
-            'types': types,
-            'categories': categories
-        }
         self.score = 0
         self.current_question = 0
 
-    def fetch_questions(self, total_questions=10, category=18, question_type='boolean'):
-        url = f"https://opentdb.com/api.php?amount={total_questions}&category={category}&type={question_type}"
+    def fetch_questions(self, amount=10, category=18, question_type='boolean'):
+        '''Call the Trivia API and format the response to fit the needs of the game'''
+        url = f"https://opentdb.com/api.php?amount={amount}&category={category}&type={question_type}"
         response = requests.get(url)
 
+        # convert raw response data to Python and pull out the list at the key of 'results'
         results = response.json()['results']
 
-        # if not results:
-        #     "ERROR"
         questions = []
-        for datum in results:
+        for result in results:
             question = {
-                'text': html.unescape(datum['question']),
-                'correct_answer': datum['correct_answer'],
-                'incorrect_answers': datum['incorrect_answers'][0] if datum['type'] == 'boolean' else datum['incorrect_answers'], # ternary
-                'category': datum['category'],
-                'type': datum['type'],
-                'user_answer': None,
-                'was_answered_correctly': False
+                'text': html.unescape(result['question']), # format the text of the question
+                'correct_answer': result['correct_answer'],
+                'incorrect_answers': result['incorrect_answers'][0], # pull the string out of the list
+                'category': result['category'],
+                'type': result['type'],
+                'user_answer': None, # to track the user's answer to this question
+                'was_answered_correctly': False # depicts whether the question was answered correctly by the user
             }
+
+            # add the question to the list of questions
             questions.append(question)
+
         return questions
-        
-    def answer_is_correct(self, question, answer_attempt):
-        '''Return a boolean indicating if the answer_attempt is the correct_answer'''
-        if question['type'] == 'boolean':
-            return question['correct_answer'] == answer_attempt
+
+    def answer_is_correct(self, question, user_answer):
+        '''Return a boolean indicating if the user_answer is the correct_answer for the question'''
+        return question['correct_answer'] == user_answer
+
 
     @property
     def total_questions(self):
+        '''Return the length of the questions list'''
         return len(self.questions)
 
     def __str__(self):
-        return f'Question {self.current_question + 1} of {len(self.questions)}'
+        return f'Question {self.current_question + 1} of {self.total_questions}'
+
 
 
 if __name__ == '__main__':
-    def get_custom_settings():
-        choices = {
-            '1': 'Default settings',
-            '2': 'Custom settings'
-        }
+    game = Trivia() # instatiate the Trivia class
 
-        # print("Choose settings:")
-        # for label, choice in choices.items():
-        #     print(f"{label}. {choice}")
+    for question in game.questions:
+        print(game)
+        print(question['text'])
+        game.current_question += 1 # increment question count
+        
+        # ask the user for their answer
+        answer = input("True or False: ").capitalize() # true -> True, fAlSe -> False
 
-    def v1():
-        while True:
-            game = Trivia()
-
-            print("Welcome to Trivia!\n")
-
-            for question in game.questions:
-                print(game)
-                print(question['text'])
-                game.current_question += 1
-
-                answer = input("True or False: ")
-                print()
-
-                if game.answer_is_correct(question, answer):
-                    game.score += 1
-                    question['was_answered_correctly'] = True
-
-            
-            message = f'You got {game.score} out of {game.total_questions} correct!'
-            if game.score < 4:
-                message += '\nBetter luck next time!'
-            elif game.score < 7:
-                message += '\nGood work.'
-            elif game.score <= 10:
-                message += '\nWell done!'
-
-            print(message)
+        # ensure the user is entering True or False
+        while answer not in ['True', 'False']:
+            print("Valid responses: True, False")
+            answer = input("True or False: ").capitalize() # true -> True, fAlSe -> False
 
 
-            show_results = input("\nDo you want to see your results? y/n: ")
-            if show_results == 'y':
-                for question in game.questions:
-                    symbol = "x" if question['was_answered_correctly'] == True else "+"
-                    print(f"{symbol} | {question['text']}")
-            
+        print()
 
+        if game.answer_is_correct(question, answer):
+            game.score += 1
+            question['was_answered_correctly'] = True
 
-            again = input("\nDo you want to play again? y/n: ")
-            if again == 'y':
-                print("Okay, let's play again!")
+    # display the outcome of the game to the user
+    message = f'You got {game.score} out of {game.total_questions} correct!'
+    print(message)
 
-            elif again == 'n':
-                print("Goodbye")
-                break
-
-    def v2():
-        pass
-
-
-v1()
+    show_results = input("\nDo you want to see your results? y/n: ")
+    if show_results == 'y':
+        for question in game.questions:
+            symbol = "x" if question['was_answered_correctly'] else "+" # ternary if statements
+            print(f"{symbol} | {question['text']}")
