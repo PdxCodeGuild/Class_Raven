f1c3571c0aee3ef0817fc7dddc38dc97e9e67ee0
James Johnson <jamesjhnsn1995@gmail.com>
2021-11-09 19:09:09 -0800

    final_mob_lab

--- Code/James/james_jose_michael.py
+++ Code/James/james_jose_michael.py
@@ -1,109 +1,94 @@
-import random
-    
-    
-class Character():
-    def __init__(self, power= 10, name= 'Jeff', health= 10, x = 1, y = 3, experience= 0):
-        self.x = x
-        self.y = y
-        self.power = power
-        self.name = name
-        self.health = health
-        self.experience = experience
-    
-class Enemy(Character):
-    def __init__(self, power= 10, name= 'Tom', health= 10, x = 9, y = 3, coins=0):
-        super().__init__(name, health, power, x, y)
-        self.coins = coins
-       
-    ...
-class Board():
-    def __init__(self, width=10, height=10, board=[]):
-        self.width = width
-        self.height = height
-        self.board = board
-        for i in range(height):
-            board.append([])
-            for j in range(width):
-                board[i].append(' ')
-        
-        
-board = Board()
-player = Character('Player', 100, 20, 0, 0)
-player.x=4
-player.y=4
-enemy = Enemy()
-enemy.coins = random.randint(0, 100)
-player.experience = 0
-enemies = int(input("How many enemies do you want to fight? "))
-for i in range(0, enemies):
-    enemy.x = random.randint(0, board.height - 1)
-    enemy.y = random.randint(0, board.width - 1)
-    board.board[enemy.x][enemy.y] = '§'
-action = 'None'
+"""  Trivia API """
+import html
+import requests
 
-while True:
-    if board.board[player.x][player.y] == '§':
-        print('You\'ve encountered an enemy!')
-        action = input('what will you do? ')
-    if action == 'attack':
-        print('You\'ve slain the enemy')
-        board.board[player.x][player.y] = ' '  # remove the enemy from the board
-        enemies -= 1
-        player.experience += enemy.coins # add experience
-        print('You have gained ' + str(enemy.coins) + ' experience')
-        action = 'None' # reset action
-        if enemies == 0:
-            print('Congrats you won!')
-            print('You have ' + str(player.experience) + ' experience')
-            break
-    
-    elif not board.board[player.x][player.y] == '§':
-        pass
-    
-    else:
-        print('You hestitated and were slain')
-        break
+class trivia:
+    def __init__(self):
+        self.info = []
+        self.correct_answers = []
+        self.i = 0
+        self.points = 0
+
+    def load(self):
+        response = requests.get('https://opentdb.com/api.php?amount=10&category=23&type=boolean', params={'format': 'json'})
+        #print(response.text)
+        contents = response.json() # this is python dictionary
+        # now get question out of the dictionary
+        questions = contents['results'] # gives all the data from the api
         
-    for i in range(board.height):
-        for j in range(board.width):
-            # if we're at the player location, print the player icon
-            if i == player.x and j == player.y:
-                print('☺', end=' ')
-            else:
-                print(board.board[i][j], end=' ')  # otherwise print the board square
-        print()
+        self.info = questions # now the class is initialized
         
         
+
+        
+        
+        
+    def questions_answers(self):    
+       
+       for question in self.info:
+            #print(html.unescape(question['question']))
+            self.correct_answers.append(html.unescape(question['correct_answer'])) # questoin is just an iterable its not important
             
     
-    command = input('what is your command? ')  # get the command from the user
-    
-    if command == 'done':
-        break  # exit the game
-    elif command == 'left':
-        if not player.y == 0:
-            player.y -= 1# move left
-        else:
-            print('Out of range go another direction.')  
+    def answer_checker(self, user_answer):
+           
+        if user_answer == self.correct_answers[self.i]:
+            print('Correct')
+            self.i += 1
+            self.points += 1
+            return True
+            
+
+        elif user_answer != self.correct_answers[self.i]:
+            print('Incorrect')
+            self.i += 1
+            return False
+            
         
-    elif command == 'right':
-        if not player.y >= 9:
-            player.y += 1  # move right
-        else:
-            player.y = 9
-            print('Out of range go another direction.')  # move right
+
+            
+            
+            
+        
     
-    elif command == 'up':
-        if player.x == 0:
-            print('Out of range go another direction.')
-        else:
-            player.x -= 1  # move up
     
-    elif command == 'down':
-        if player.x == 9:
-            print('Out of range go another direction.')
-        else:
-            player.x += 1  # move down
+trivia = trivia() # create instance of our class
+trivia.load()
+trivia.questions_answers()
+
+
+
+print('Welcome to history trivia enter True or False for the following questions.')
+counter = 0
+#points = 0
+play = True
+
+while play:
+    print(trivia.correct_answers)
+    print(html.unescape(trivia.info[counter]['question']))
+    user_answer = input('Enter True or False: ').capitalize()
+    
+    if user_answer not in  ['True', 'False']:
+        print('Please enter True or False only.')
+        continue
+    
+    trivia.answer_checker(user_answer)
+    
+    counter += 1
+    
+    
+    if counter == 9:
+        print(f'You answered {trivia.points} questions correctly')
+        play_again = (input('Do you want to play again type yes or no \n>> ')).lower()
+        if play_again == 'no':
+            play = False
+        
+        elif play_again == 'yes':
+            trivia.load()
+            counter = 0
+           
+            
+
+
 
     
-    print('enemies remaining:', enemies)
