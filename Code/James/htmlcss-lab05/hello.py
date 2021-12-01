6461de477451f796943f80e3365f40a5d9b3cfc7
James Johnson <jamesjhnsn1995@gmail.com>
2021-11-30 20:38:52 -0800

    incomplete

--- Code/James/htmlcss-lab05/hello.py
+++ Code/James/htmlcss-lab05/hello.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index1():
+    
+    return render_template('index.html')
 
+@app.route('/login')
+def login():
+    # if request.method == 'POST':
+    #    print("we posted")
+    return render_template('login.html')
+
+
+
+app.run(debug=True)
