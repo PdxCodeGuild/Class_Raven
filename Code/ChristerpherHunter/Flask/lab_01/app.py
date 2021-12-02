3eb2ae44822ec392a99b4ee150db0b193b536c6a
Christerpher Hunter <djhunter67@gmail.com>
2021-12-01 20:32:19 -0500

    Added the clear form button successfully

--- Code/ChristerpherHunter/Flask/lab_01/app.py
+++ Code/ChristerpherHunter/Flask/lab_01/app.py
@@ -1,27 +1,40 @@
 from flask import Flask, render_template, request, redirect, url_for
-from json import dumps, loads
+from json import dumps, loads, decoder
 
 app = Flask(__name__)
+
+db = "lab_01/db.json"
 
 
 def load_data() -> dict():
     """Load the data to be excercised"""
 
-    with open("lab_01/db.json", "r") as f_read:
+    with open(db, "r") as f_read:
         return loads(f_read.read())
+
+
+def clear_order() -> None:
+    """Remove the data in the json db"""
+
+    with open(db, "r+") as f_clear:
+        f_clear.truncate(0)
 
 
 def save_data(data: dict) -> None:
     """Save the data input from the user"""
 
-    with open("lab_01/db.json", "w") as f_write:
+    with open(db, "w") as f_write:
         f_write.write(dumps(data, indent=2))
 
 
 @app.route("/")
 def index():
 
-    return render_template("index.html", orders=load_data())
+    try:
+        return render_template("index.html", orders=load_data())
+    except decoder.JSONDecodeError:
+        null_info = {"No Oders Placed": ""}
+        return render_template("index.html", orders=null_info)
 
 
 @app.route("/orders", methods=['POST', 'GET'])
@@ -41,6 +54,15 @@ def orders():
 
     save_data(order_dict)
     # print(load_data())
+
+    return redirect(url_for('index'))
+
+
+@app.route("/clear", methods=['POST'])
+def reset_receipt():
+    """"Clear the db when the clear button is pressed"""
+
+    clear_order()
 
     return redirect(url_for('index'))
 
