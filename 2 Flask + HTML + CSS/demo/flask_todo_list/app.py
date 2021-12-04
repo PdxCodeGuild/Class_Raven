9ea0799fa8c069b8a8969d3433b838bb326dcd53
Keegan Good <keegood8@gmail.com>
2021-12-03 18:24:51 -0800

    finished flask todo app

--- 2 Flask + HTML + CSS/demo/flask_todo_list/app.py
+++ 2 Flask + HTML + CSS/demo/flask_todo_list/app.py
@@ -15,6 +15,14 @@ def save_data(filename, data):
 
     return
 
+
+def find_todo(todos, todo_id):
+    # loop through the todos list to find the todo with the given todo_id
+    for todo_index, todo in enumerate(todos):
+        if todo['id'] == todo_id:
+            return (todo_index, todo)
+
+
 JSON_DB = './static/todos.json'
 
 
@@ -39,6 +47,11 @@ def create_todo():
     # when receiving user's form data for a new todo item, 
     # process it and redirect to index page
     elif request.method == 'POST':
+
+        # if the form text is blank, return to the template with an error message
+        if not request.form['text']:
+            return render_template('create.html', error="Text cannot be blank!")
+
         # load all todo items from the database
         todos = load_data(JSON_DB)
 
@@ -54,6 +67,47 @@ def create_todo():
         save_data(JSON_DB, todos)
 
         return redirect(url_for('index'))
+
+
+@app.route('/update/<int:todo_id>', methods=['GET', 'POST'])
+def update_todo(todo_id):
+    todos = load_data(JSON_DB)
+
+    # unpack the tuple into two separate variables
+    todo_index, todo = find_todo(todos, todo_id)
+
+    if request.method == 'GET':
+        return render_template('update.html', todo=todo)
+
+    elif request.method == 'POST':
+        # if the form text is blank, return to the template with an error message
+        if not request.form['text']:
+            return render_template('update.html', error="Text cannot be blank!", todo=todo)
+        
+        # update the text
+        todo.update({
+            'text': request.form['text']
+        })
+
+        save_data(JSON_DB, todos)
+
+        return redirect(url_for('index'))
+
+@app.route('/delete/<int:todo_id>')
+def delete_todo(todo_id):
+    todos = load_data(JSON_DB)
+
+    todo_index, todo = find_todo(todos, todo_id)
+
+    todos.pop(todo_index-1)
+
+    save_data(JSON_DB, todos)
+    
+    return redirect(url_for('index'))
+
+
+
+
 
 @app.route('/toggle-complete/<int:todo_id>')
 def toggle_complete(todo_id):
