4f6ccc3274238da579998f3f57a312ad5898a4c8
Christerpher Hunter <djhunter67@gmail.com>
2021-12-04 00:05:48 -0500

    Major progress on layout, starting on logic

--- Code/ChristerpherHunter/Flask/lab_02/pw_genny/routes.py
+++ Code/ChristerpherHunter/Flask/lab_02/pw_genny/routes.py
@@ -3,7 +3,20 @@ from pw_genny.models import Data
 from pw_genny import app
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
 
     return render_template("index.html")
+
+
+@app.route("/enter_pw", methods=["GET", "POST"])
+def entry():
+
+    return render_template("entry.html", bitty_bet=['q', 's', 'e', 'd', 'r', 'g', 'y', 'y', 'h', 'j'])
+
+
+@app.route("/execute", methods=['GET', 'POST'])
+def execute():
+    """Execute the password generation"""
+
+    return redirect(url_for("entry"))
