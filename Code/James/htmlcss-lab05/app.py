44f0be3bbba504fcf932c039f25418c767b08b76
James Johnson <jamesjhnsn1995@gmail.com>
2021-12-03 18:31:08 -0800

    progress

--- Code/James/htmlcss-lab05/app.py
+++ Code/James/htmlcss-lab05/app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import json
 app = Flask(__name__)
 
+def load_data(filename):
+    with open(filename, 'r') as json_file:
+        return json.loads(json_file.read())
+
+def save_data(filename, data):
+    with open(filename, 'w') as json_file:
+        json_file.write(json.dumps(data, indent=2))
+
+    return
 
 @app.route('/')
 def index():
@@ -9,36 +18,59 @@ def index():
     return render_template('index.html')
 
 
-
-
 @app.route('/login', methods=["POST", "GET"])
 def login():
-    if request.method =="GET":  
+    if request.method == "GET":
         return render_template("login.html")
-         
 
-    else: 
-        
+    else:
+
         user = request.form["fname"]
         user2 = request.form["lname"]
-        return f'Welcome! {user} {user2}'
+        return f'Welcome! {user} {user2}' 
+        
 
 
 @app.route('/order', methods=["POST", "GET"])
 def food_order():
-    if request.method == "POST":
-        meat = request.form['name']
-        return f'<h1> You ordered {meat} </h1>'
-    else:
-        meat_options = ['Carnitas', 'Chicken', 'Sofritas', 'None' ]
-        return render_template("order.html", meat_options = meat_options)
+    if request.method == "GET":
+        categories =  ['tortilla', 'rice', 'bean', 'protein', 'additional_ingredients' ]
+        options = {
+            'tortilla': ['White Flour', 'Wheat Flour', 'Spinach', 'Corn'],
+            'rice' : ['White Rice', 'Brown Rice'],
+            'bean': ['Black Beans', 'Pinto Beans'],
+            'protein' : ['Carnitas', 'Chicken', 'Sofritas', 'None'],
+            'additional_ingredients' : ['Cheese', 'Sour Cream']
+        }
+        images = ['tortila', 'rice', 'bean', 'protein', 'additional_ingredients']
+
+
+
+
+        return render_template("order.html", options=options, categories=categories, images=images)
+
+    elif request.method == "POST":
         
+        return render_template("receipt.html")
 
-# @app.route("/<usr>")
-# def user(usr, usr2):
-#     return f"<h1>Hello! {usr} {usr2} </h1>"
 
-    
+@app.route('/receipt', methods=["POST", "GET"])
+
+def receipt():
+    if request.method == "GET":
+        
+        return f'wlkefjaldkjflakdj'
+
+    elif  request.method == "POST":
+          cost = {
+            'tortilla': 2,
+            'rice': 3,
+            'bean': 1,
+            'protein' : 3,
+            'additional_ingredients': .5
+        } 
+        
+        return render_template("receipt.html") 
 
 
 if __name__ == "__main__":
