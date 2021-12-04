a64453f691d0271ed0559131806d571c0873f395
Christerpher Hunter <djhunter67@gmail.com>
2021-12-04 16:45:42 -0500

    minor refactoring, make the pw generator

--- Code/ChristerpherHunter/Flask/lab_02/pw_genny/__init__.py
+++ Code/ChristerpherHunter/Flask/lab_02/pw_genny/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "12$KMB0IMz2qMrQ0l4.o66Sxuz11KPc5FY4eXmJ6HabOoweuA2ehH172"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from pw_genny import routes
--- Code/ChristerpherHunter/Flask/lab_02/pw_genny/routes.py
+++ Code/ChristerpherHunter/Flask/lab_02/pw_genny/routes.py
@@ -1,7 +1,14 @@
 from flask import render_template, request, redirect, url_for
 from pw_genny.models import Data
-from pw_genny import app
+from pw_genny import app, db, bcrypt
 
+
+def password_genny(low_lets: int, up_lets: int, num_of_numbers: int, spec_chars: int) -> str:
+    """Generate a password based on the user input"""
+
+    pw_length = str(low_lets + up_lets + num_of_numbers + spec_chars)
+
+    return f"Password Length: {pw_length}"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -9,14 +16,20 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/enter_pw", methods=["GET", "POST"])
-def entry():
-
-    return render_template("entry.html", bitty_bet=['q', 's', 'e', 'd', 'r', 'g', 'y', 'y', 'h', 'j'])
-
-
-@app.route("/execute", methods=['GET', 'POST'])
-def execute():
+@app.route("/show_pw", methods=["POST"])
+def show_pw():
     """Execute the password generation"""
 
-    return redirect(url_for("entry"))
+    num_of_lowercase_letters = int(request.form["lw-letters"])
+    num_of_capital_letters = int(request.form["up-letters"])
+    num_of_numbers = int(request.form["num-nums"])
+    num_of_special_characters = int(request.form["spec-chars"])
+
+    p_word = password_genny(
+        low_lets=num_of_lowercase_letters,
+        up_lets=num_of_capital_letters,
+        num_of_numbers=num_of_numbers,
+        spec_chars=num_of_special_characters
+    )
+
+    return render_template("show_page.html", p_word=p_word)
