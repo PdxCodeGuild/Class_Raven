9e9aa13dbaf04ef5fc59fdfbd4ebe09d6cb53927
arek8675 <smullenarek10@gmail.com>
2021-12-03 18:59:58 -0800

    Saving Changes

--- Code/Arek/html-css/lab6/app.py
+++ Code/Arek/html-css/lab6/app.py
@@ -1,8 +1,13 @@
 #redoing the password generator lab with flask and html/css
 
 from flask import Flask, app, render_template, request, url_for
-
+from random import choice
+from string import ascii_lowercase,ascii_uppercase,digits,punctuation
 app = Flask(__name__)
+
+
+
+
 
 @app.route('/')
 def index():
