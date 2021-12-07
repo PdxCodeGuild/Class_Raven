872261cede50a08dd89ef342670bb14b2bce6d47
arek8675 <smullenarek10@gmail.com>
2021-12-06 18:50:05 -0800

    Saving changes

--- Code/Arek/html-css/lab6/app.py
+++ Code/Arek/html-css/lab6/app.py
@@ -1,16 +1,42 @@
 #redoing the password generator lab with flask and html/css
 
 from flask import Flask, app, render_template, request, url_for
-from random import choice
+from random import choice,shuffle
 from string import ascii_lowercase,ascii_uppercase,digits,punctuation
 app = Flask(__name__)
 
 
+def make_pass(upper,lower,punct,nums):
+    new_pass = []
+    total = (int(upper) + int(lower) + int(punct) + int(nums))
+    check = 0
+
+    while check != total:
+        if check < upper:
+            new_pass.append(choice(list(ascii_uppercase)))
+            check += 1
+        elif check < upper + lower:
+            new_pass.append(choice(list(ascii_lowercase)))
+            check += 1
+        elif check < upper + lower + punct:
+            new_pass.append(choice(list(punctuation)))
+            check += 1
+        elif check < total:
+            new_pass.append(choice(list(digits)))
+            check += 1
+    shuffle(new_pass)
+    new_pass = ''.join(new_pass)
+    return new_pass
+        
+            
 
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        password = make_pass(request.form['uppercase'],request.form['lowercase'],request.form['punctuation'],request.form['numbers'])
+        return render_template('index.html', password=password)
 
 app.run(debug=True)
