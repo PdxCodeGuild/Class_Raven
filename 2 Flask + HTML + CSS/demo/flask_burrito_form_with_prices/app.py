37bd63d85a6019c05382870b3b8a1fa9553ce07b
Keegan Good <keegood8@gmail.com>
2021-12-08 10:01:07 -0800

    added comments explaining process_order view

--- 2 Flask + HTML + CSS/demo/flask_burrito_form_with_prices/app.py
+++ 2 Flask + HTML + CSS/demo/flask_burrito_form_with_prices/app.py
@@ -38,12 +38,18 @@ def process_order():
     # process the form
     for field in request.form:
         if field == 'extra':
+            # convert each extra item into a dictionary
+            # ['cheese-1.00', 'sour cream-0.50'] -> [['cheese', '1.00'],['sour cream', '0.50']]
             field_data = [item.split('-') for item in request.form.getlist('extra')]
+            
+            # convert each split list into a dict
+            # [{'name': 'cheese', 'price':'1.00}, {'name': 'sour cream', 'price': '0.50'}]
             field_data = [field_data_to_dict(item) for item in field_data]
         else:
             # value comes through as veggie-2.00. Split at the '-' is ['veggies', '2.00']
             field_data = request.form[field].split('-')
 
+            # all fields other than 'delivery' will have 2 items
             if len(field_data) > 1:
                 field_data = field_data_to_dict(field_data)
                 
@@ -51,8 +57,11 @@ def process_order():
                 order_total += float(field_data['price'])
 
             else:
+                # delivery will have a price of 0.00 which won't be displayed on the template
                 field_data = field_data_to_dict([field_data[0],'0.00'])
 
+        # add field data for the current field
+        # {'protein': {'name': 'carne asada', 'price': '3.00'}}
         receipt[field] = field_data
 
 
