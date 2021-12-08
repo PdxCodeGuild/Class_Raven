34aba5508547431b19208b9f455623f234f4349d
James Johnson <jamesjhnsn1995@gmail.com>
2021-12-08 13:16:38 -0800

    update

--- Code/James/htmlcss-lab06/app.py
+++ Code/James/htmlcss-lab06/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for 
+from flask import Flask, render_template, request, redirect, url_for
 import json
 import string
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
@@ -12,17 +13,32 @@ def hello_world():
 
 @app.route('/rot_show')
 def show_rotation():
-    
-    encrypt = list('catdog')
-    
-    
+    # creates a list with what i pass into the list function
+    encrypt = list('catdog'.upper())
+
+
+# encrypt_sort = encrypt.sort()
+
+    # here we have list of the alphabet
     uppercase_alphabet = list(string.ascii_uppercase)
 
-    if encrypt in uppercase_alphabet:
-        encrypt += 13
-    
-    
-    
-    return render_template("rot_show.html", uppercase_alphabet= uppercase_alphabet, encrypt=encrypt)
+    print(uppercase_alphabet)
+     # I want to compare each index of encrypt with the uppercase_alphabet index
 
-    
+    encrypted_message = []
+
+    index = 0
+       # i want to loop through the list for as many characters are in list,
+
+    for index in range(len(encrypt)):
+            # its only doing first index during the loop
+            # for encrypt[index] in uppercase_alphabet[index]:
+
+            # this code is appending to the encrypt list
+            # this is just changing the uppercase alphabet list
+            cipher = uppercase_alphabet.index(encrypt[index])
+            encrypted_message.append(uppercase_alphabet[(cipher + 13) % 26])
+
+            index += 1
+
+    return render_template("rot_show.html", uppercase_alphabet=uppercase_alphabet, encrypt=encrypt, encrypted_message=encrypted_message)
