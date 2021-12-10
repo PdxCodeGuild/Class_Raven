cba56c00eb9ae7fbae9c4bd4ee471aa3a1200c9e
Lee Colburn <colburn.lee@gmail.com>
2021-12-09 20:29:16 -0800

    lab1 series complete

---
+++ Code/Lee/Javascript/lab01/converter.js
@@ -0,0 +1,76 @@
+/* Lee Colburn
+Lab 1 Javascript
+Unit Converter */
+
+function getMeters(distance, unit) {
+    if (unit == "feet"){
+        let result=(distance * .3048)
+        return result
+    }
+    else if (unit == "miles") {
+        let result = (distance * 1609.34)
+        return result
+    }
+    else if (unit == "meters") {
+        let result = (distance * 1)
+        return result
+    }
+    else if (unit == "kilometers") {
+        let result = (distance * 1000)
+        return result
+    }
+    else if (unit == "yards") {
+        let result = (distance * .9144)
+        return result
+    }
+    else if (unit == "inches") {
+        let result = (distance * 0.0254)
+        return result 
+    }
+}
+
+function getCustom(meters, unit){
+    if (unit == "feet"){
+        let result=(meters / .3048)
+        return result
+    }
+    else if (unit == "miles") {
+        let result = (meters / 1609.34)
+        return result
+    }
+    else if (unit == "meters") {
+        let result = (meters / 1)
+        return result
+    }
+    else if (unit == "kilometers") {
+        let result = (meters / 1000)
+        return result
+    }
+    else if (unit == "yards") {
+        let result = (meters / .9144)
+        return result
+    }
+    else if (unit == "inches") {
+        let result = (meters / 0.0254)
+        return result 
+    }
+}
+
+
+
+let outputUnit = prompt('Enter the unit you would like to convert to: ')
+let inputDistance = prompt('Enter the distance you would like to convert:')
+let inputUnit = prompt('Enter the unit you are converting from (feet, miles, meters, kilometers, yards, or inches)')
+let meterResult = getMeters(inputDistance, inputUnit)
+
+if (outputUnit != "meters"){
+    let meters = getMeters(inputDistance, inputUnit)
+    let customResult = getCustom(meters, outputUnit)
+    alert(inputDistance + " " + inputUnit + " is equal to " + customResult + " " + outputUnit)
+} else {
+    alert(inputDistance + " " + inputUnit + " is equal to " + meterResult + " " + outputUnit)
+}
+
+
+
+
--- Code/Lee/Javascript/lab01/rps.js
+++ Code/Lee/Javascript/lab01/rps.js
@@ -1,22 +1,6 @@
-/* Rock Paper Scissors
-Let's play rock-paper-scissors with the computer. You may want to try using these emojis moyaipage_with_curlscissorsfist_raisedhandv
-
-The computer will ask the user for their choice (rock, paper, scissors)
-The computer will randomly choose rock, paper or scissors
-Determine who won and tell the user
-Let's list all the cases:
-
-rock vs rock (tie)
-rock vs paper
-rock vs scissors
-paper vs rock
-paper vs paper (tie)
-paper vs scissors
-scissors vs rock
-scissors vs paper
-scissors vs scissors (tie)
-Version 2 (optional)
-After playing, ask them if they'd like to play again. If they say yes, restart the game, otherwise exit.*/
+/* Lee Colburn
+Lab 1 Javascript
+Rock, Paper, Scissors */
 
 let options = ['rock', 'paper', 'scissors']
 
