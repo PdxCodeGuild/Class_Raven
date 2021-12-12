da6f2bbc08ea8b3d1a9f1abab3c8bb995170f39e
arek8675 <smullenarek10@gmail.com>
2021-12-11 16:48:23 -0800

    Made some cosmetic changes and also created functions for a delete button and a complete button

--- Code/Arek/Javascript/lab4/index.js
+++ Code/Arek/Javascript/lab4/index.js
@@ -4,27 +4,70 @@ let row = document.querySelector('.row')
 
 addButton.addEventListener('click', function () {
     task = prompt("Enter Task: ")
-    let todo = document.createElement('div')
+    // if cancel is clicked then a card will be created with 'null'
+    // need to do input validation as well, the user enters nothing for prompt and presses ok.
+    // It will make a card that is blank and too small
+    console.log(task)
+    var todo = document.createElement('div')
     todo.classList.add('todo')
+    todo.classList.add('col')
+    todo.classList.add('s10')
+    todo.classList.add('card')
+    todo.classList.add('purple')
+    todo.style.color = 'red';
+    
+
     todo.innerHTML = `
         
-        <div class=" col s10 card purple">
-            <div class="card-content black-text">  
-                <p>
-                    ${task}
+        
+        <div class="card-content">  
+            <p>
+                ${task}
+                <a id='delete-task' class='btn-small waves-effect waves-light black'>
+                    <i class=' material-icons center'>delete</i>
+                </a>
+                <a id='task-done' class='btn-small waves-effect waves-light grey darken-2'>
+                    <i class=' material-icons center'>check</i>
+                </a>
                         
-                </p>
+            </p>
+            
                     
-            </div>
         </div>
         
         
-        <a id='delete-task' class='col s1 btn-small waves-effect waves-light black'>
-            <i class=' material-icons center'>delete</i>
-        </a>
+      
         
                 `
+    // IF THERE ARE TWO CARDS, YOU HAVE TO DELETE THE FIRST ONE TO BE ABLE TO DELETE THE ONES UNDER
         
     row.appendChild(todo)
+    var delButton = document.querySelector('#delete-task')
+    var doneButton = document.querySelector('#task-done')
+
+    delButton.addEventListener('click', function() {
+        answer = confirm("Are you sure you want to delete this task? ")
+        if (answer == true){
+            row.removeChild(todo)
+        }
+
+    })
+
+    doneButton.addEventListener('click', function(){
+        todo.classList.replace('purple', 'yellow')
+        //NEED TO FIND A WAY TO USE THE STRIKE() METHOD TO MAKE THE TEST
+        // HAVE A LINE THROUGH IT
+
+        // if you have more than one cards it will make them all yellow
+
+
+
+
+
+    })
 
 })
+
+
+
+
