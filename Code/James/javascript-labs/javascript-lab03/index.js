544f74ec8ae257c86d55cc69f47d9d93f0a89416
James Johnson <jamesjhnsn1995@gmail.com>
2021-12-11 22:12:24 -0800

    finished

--- Code/James/javascript-labs/javascript-lab03/index.js
+++ Code/James/javascript-labs/javascript-lab03/index.js
@@ -3,8 +3,6 @@ console.log(user);
 
 
 
-
-
 let userNumList = user.split('')
 
 userNumList = userNumList.map((number)=> parseInt(number))
@@ -13,21 +11,43 @@ console.log(userNumList);
 
 let lastDigit = userNumList.pop()
 
+
 console.log(lastDigit);
 
 let reverseList = userNumList.reverse()
 console.log(reverseList);
 
 
-for (let i = 1; i<reverseList.length; i+=2){
-   reverseList[i] = Math.pow(reverseList[i], 2)
+for (let i = 0; i<reverseList.length; i+=2){
+   reverseList[i] = reverseList[i]*2
 
 }
 
 console.log(reverseList);
 
-// console.log(reverseList);
-// console.log(doubleOther);
+
+for (let i = 0; i<reverseList.length; i++){
+    if (reverseList[i]> 9){
+        reverseList[i] = reverseList[i]-9
+    }
+}
+console.log(reverseList);
+
+let sum = reverseList.reduce((previousValue, currentValue)=>{
+    return previousValue + currentValue
+
+}, 0)
+console.log(sum)
+
+let sumDigit = Math.floor((sum / 1) % 10)
+console.log(sumDigit);
+
+if (sumDigit == lastDigit){
+    alert('Valid!')
+    
+}
+
+
 
 
 
