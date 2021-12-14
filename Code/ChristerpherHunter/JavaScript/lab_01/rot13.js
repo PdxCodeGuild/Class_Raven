1cd75100499842452d183974af2a8a2bc0b977d6
Christerpher Hunter <djhunter67@gmail.com>
2021-12-13 23:52:49 -0500

    a couple of hours and made zero progress on the ROT cipher

--- Code/ChristerpherHunter/JavaScript/lab_01/rot13.js
+++ Code/ChristerpherHunter/JavaScript/lab_01/rot13.js
@@ -1,7 +1,49 @@
 
 
-const userString = "Hello World"
+const userString = "Hello World!"
 
 // userString = prompt("Please enter the word to be encrypted: ")
 
+var n = 13
+
 const asciiLowerCase = "abcdefghijklmnopqrstuvwxyz"
+
+// let asciiLowerCase = {1: "a",
+//                       2: "b",
+//                       3: "c",
+//                       4: "d",
+//                       5: "e",
+//                       6: "f",
+//                       7: "g",
+//                       8: "h",
+//                       9: "i",
+//                       10: "j",
+//                       11: "k",
+//                       12: "l",
+//                       13: "m",
+//                       14: "n",
+//                       15: "o",
+//                       16: "p",
+//                       17: "q",
+//                       18: "r",
+//                       19: "s",
+//                       20: "t",
+//                       21: "u",
+//                       22: "v",
+//                       23: "w",
+//                       24: "x",
+//                       25: "y",
+//                       26: "z"
+// }
+
+let wordArray = userString.split(" ")
+console.log(wordArray)
+
+// for (letter of userString) {
+//     console.log(letter.toLowerCase().charCodeAt(letter));
+//     // if (letter === asciiLowerCase[letter]) {
+//     //     console.log("working")
+//     // } else {
+//     //     console.log("Not Working")
+//     // }
+// }
