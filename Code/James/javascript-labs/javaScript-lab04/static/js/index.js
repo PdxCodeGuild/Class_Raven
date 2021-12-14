95ea0dae7bd54715d56615fc40dd31f6a9bb59ac
James Johnson <jamesjhnsn1995@gmail.com>
2021-12-13 21:15:55 -0800

    finished

--- Code/James/javascript-labs/javaScript-lab04/static/js/index.js
+++ Code/James/javascript-labs/javaScript-lab04/static/js/index.js
@@ -1,18 +1,111 @@
-let trash = document.getElementById('trash')
-let container = document.getElementById('.container')
-let add = document.getElementById('add')
-let text = document.getElementById('#text')
-// trash.addEventListener('click', function(){
-//     trash.classList.add('grow')
-// })
+let trash = document.querySelector("trash");
 
-// trash.addEventListener('click', function (){
-//     let list = document.createElement('div')
-//     list.classList.add('list')
-// })
+let completeTodoSection = document.querySelector("#complete-todos"),
+  incompleteTodoSection = document.querySelector("#incomplete-todos"),
+  submitButton = document.querySelector("#submit"),
+  inputField = document.querySelector("#text");
 
+let todos = [
+  {
+    text: "Mow the lawn",
+    complete: false,
+  },
+  {
+    text: "Walk the dog",
+    complete: false,
+  },
+  {
+    text: "Groceries",
+    complete: true,
+  },
+];
 
-add.addEventListener('click', function (){
-    var txt = prompt('Task to do is..');
-    text.innerHTML += '<li class=list-group-item'> + txt + '</li>'
-})
+submitButton.addEventListener("click", () => {
+  addTodo(inputField.value);
+  inputField.value = "";
+  updateTodoList();
+});
+
+function addTodo(text) {
+  let newTodo = {
+    text: text,
+    completed: false,
+  };
+
+  todos = todos.concat(newTodo);
+}
+
+function toggleComplete(targetTodo) {
+  todos = todos.map((todo) => {
+    if (todo.text === targetTodo.text) {
+      return {
+        text: todo.text,
+        complete: !todo.complete,
+      };
+    } else {
+      return todo;
+    }
+  });
+}
+
+function deleteTodo(targetTodo) {
+  todos = todos.filter((todo) => {
+    if (todo.text === targetTodo.text) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+}
+
+function generateTodoItem(todo) {
+  let todoItem, crudButtons;
+
+  todoItem = document.createElement("div");
+  todoItem.innerHTML = `<div>${todo.text}</div>`;
+
+  crudButtons = document.createElement("div");
+
+  let completeButton = document.createElement("button");
+  let iconComplete = '<i class="fas fa-check fa-lg"></i>';
+  completeButton.innerHTML = iconComplete;
+  completeButton.addEventListener("click", () => {
+    toggleComplete(todo);
+    updateTodoList();
+  });
+
+  let iconDelete = '<i class="far fa-trash-alt fa-lg"></i>';
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = iconDelete;
+  deleteButton.addEventListener("click", () => {
+    deleteTodo(todo);
+    updateTodoList();
+  });
+
+  crudButtons.appendChild(completeButton);
+  crudButtons.appendChild(deleteButton);
+  todoItem.appendChild(crudButtons);
+
+  return todoItem;
+}
+
+function updateTodoList() {
+  let todoItem;
+
+  completeTodoSection.innerHTML = incompleteTodoSection.innerHTML = "";
+
+  todos.forEach((todo) => {
+    todoItem = generateTodoItem(todo);
+
+    if (todo.complete) {
+      todoItem.classList.add("complete", "todo-item");
+      completeTodoSection.appendChild(todoItem);
+    } else {
+      todoItem.classList.add("todo-item");
+
+      incompleteTodoSection.appendChild(todoItem);
+    }
+  });
+}
+
+updateTodoList();
