66e1749c15fb75afd87c93bd304b469ff0c1b752
cdmichaelb <broetjem@gmail.com>
2021-12-14 16:43:11 -0800

    commenting

--- Code/Michael/javascript/lab03/assets/index.js
+++ Code/Michael/javascript/lab03/assets/index.js
@@ -7,6 +7,7 @@ function getInput() {
 		stringToEncrypt == undefined ||
 		stringToEncrypt == " "
 	) {
+		// Check if input is empty
 		stringToEncrypt = prompt("Enter a string to be encrypted: ");
 	}
 
@@ -16,6 +17,7 @@ function getInput() {
 		intNumberRotations == undefined ||
 		isNaN(intNumberRotations)
 	) {
+		// Check if input is empty
 		intNumberRotations = parseInt(prompt("Enter the number of rotations: "));
 	}
 }
@@ -25,13 +27,16 @@ function rot13(str, rotations) {
 	for (let i = 0; i < str.length; i++) {
 		let charCode = str.charCodeAt(i);
 		if (charCode >= 65 && charCode <= 90) {
+			// Uppercase
 			charCode = ((charCode - 65 + rotations) % 26) + 65;
 		} else if (charCode >= 97 && charCode <= 122) {
+			// Lowercase
 			charCode = ((charCode - 97 + rotations) % 26) + 97;
 		} else if (charCode >= 48 && charCode <= 57) {
+			// Numbers
 			charCode = ((charCode - 48 + rotations) % 10) + 48;
 		}
-		result += String.fromCharCode(charCode);
+		result += String.fromCharCode(charCode); // Convert back to string
 	}
 	return result;
 }
