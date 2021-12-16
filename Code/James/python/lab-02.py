3dbe3f4dba07e7cca9b075fb248d812a827ba78f
James Johnson <jamesjhnsn1995@gmail.com>
2021-12-16 09:02:21 -0800

    version 2 lab-02 python

--- Code/James/python/lab-02.py
+++ Code/James/python/lab-02.py
@@ -5,15 +5,25 @@ coins = [
     ('nickel', 5),
     ('penny', 1)
 ]
-dollar_amount = int(input("Enter a dollar amount: "))
+
+
+dollar_amount = float(input("Enter a dollar amount: "))
+
+dollar_amount = dollar_amount * 100
+
+
+quarter = int(dollar_amount // coins[1][1])
+total = dollar_amount - (quarter * coins[1][1])
+
+dime = int(total // coins[2][1])
+total = total - (dime * coins[2][1])
+
+nickel = int(total // coins[3][1])
+
+total = total - (nickel * coins[3][1])
+penny = int(total // coins[4][1])
 
 
 
-quarter = dollar_amount // coins[1][1]
-dime = dollar_amount // coins[2][1]
-nickel = dollar_amount // coins[3][1]
-penny = dollar_amount // coins[4][1]
-
-total = dollar_amount // quarter
-
-print(penny)
+print(
+    f'You have {quarter} quarters, {dime} dimes, {nickel} nickels, {penny} pennies.')
