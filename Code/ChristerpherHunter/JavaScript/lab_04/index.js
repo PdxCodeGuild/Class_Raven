f41778cecc1301e2330aa8f16ae72b04ee859e19
Christerpher Hunter <djhunter67@gmail.com>
2021-12-16 00:10:18 -0500

    Further refined the layout and started the JS

--- Code/ChristerpherHunter/JavaScript/lab_04/index.js
+++ Code/ChristerpherHunter/JavaScript/lab_04/index.js
@@ -1,5 +1,33 @@
+// Manipulate the DOM!
+let taskContent = document.querySelector("#content")
+let taskTopic = document.querySelector("#subTitle")
+let taskTitle = document.querySelector("#title")
+let createTask = document.querySelector("#submit")
 
 
-toDos = {
-    
+
+let toDos = [
+
+]
+
+
+// Listen for the click event
+createTask.addEventListener("click", () => {
+    createTodo(taskTitle.value, taskTopic.value, taskContent.value)
+    // Clears the field
+    taskContent.value = ""
+    taskTopic.value = ""
+    taskTitle.value = ""
+})
+
+function createTodo(title, topic, body) {
+    let createTodoTask = {
+        title: title,
+        topic: topic,
+        body: body
+    }
+
+    toDos = toDos.concat(createTodoTask)
 }
+
+console.log(toDos)
