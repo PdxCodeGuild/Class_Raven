fab2e973d210fb2ff30978ef7c96263e553f798a
James Johnson <jamesjhnsn1995@gmail.com>
2021-12-16 22:25:10 -0800

    finished lab-05

--- Code/James/javascript-labs/javascript-lab05/static/js/index.js
+++ Code/James/javascript-labs/javascript-lab05/static/js/index.js
@@ -1,47 +1,64 @@
-let button = document.querySelector('#button-quote'),
-    button2 = document.querySelector('#button-page'),
-    h1 = document.querySelector('#result'),
-    h3 = document.querySelector('#author')
-var pageNumber = pageNumber =1
+let button = document.querySelector("#button-quote"),
+  button2 = document.querySelector("#button-page"),
+  h1 = document.querySelector("#result"),
+  h3 = document.querySelector("#author"),
+  pageNumber = 1,
+  quotesList = [],
+  i = 0;
 
-
-
-const url = `https://favqs.com/api/quotes?page=${1}`
+const url = `https://favqs.com/api/quotes?page=${pageNumber}`;
 
 const headers = {
-    Accept: 'application/json'
-}
-button2.addEventListener('click', ()=>{
-    pageNumber +=1
+  Accept: "application/json",
+  Authorization: `Token token=${FAVQS_API_KEY}`,
+};
 
-})
+button.addEventListener("click", () => {
+  getQuote();
+});
 
+button2.addEventListener("click", () => {
+  i += 1;
 
-button.addEventListener('click', ()=>{
-    fetchQuote()
-})
+//   if i is less than the total list length run this code black
+  if (i < 25) {
+    h3.innerHTML = quotesList[i].author;
+    h1.innerHTML = quotesList[i].body;
+    console.log(i);
+    // quotesList = []
+  } 
+  
+//   else set i back to 0 and empty the quotes list then run the request function to start over with new list 
+  else {
+    i = 0;
+    quotesList = [];
+    getQuote();
+  }
+});
 
-function fetchQuote(){
-fetch(url, {
-    method: 'GET',
-    headers: headers
-})
-
-    .then(result => {
-        // quotes = {
-        //     id: result.quote.id,
-        //     author: result.quote.author,
-        //     quote: result.quote.body
-        // }
-        // console.log(result.quote.id);
-        // console.log(result.quote.author);
-        // console.log(result.quote.body);
-
-        // h1.innerHTML = result.quote.body
-        // h3.innerHTML = result.quote.author
-
-        console.log(result);
+const params = {
+  page: pageNumber,
+};
+function getQuote() {
+  axios
+    .get(url, {
+      headers: headers,
+      params: params,
     })
 
-    .catch(error => console.log('error!', error))
+    .then((response) => {
+    //   set i back to zero when function is called outside of the button 2 function
+     i = 0;
+      quotesList = [];
+      let quotes = response.data.quotes;
+      let data = response.data;
+      console.log(data);
+      console.log(quotes);
+      quotes.forEach((quote) => {
+        quotesList.push(quote);
+        h3.innerHTML = quotes[i].author;
+        h1.innerHTML = quotes[i].body;
+      });
+    })
+    .catch((error) => console.log("error!", error));
 }
