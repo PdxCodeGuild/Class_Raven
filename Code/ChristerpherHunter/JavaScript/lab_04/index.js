1c5345cba7942aa607797f6897b1bd221dbf69d9
Christerpher Hunter <djhunter67@gmail.com>
2021-12-17 22:01:18 -0500

    minor progress this evening

--- Code/ChristerpherHunter/JavaScript/lab_04/index.js
+++ Code/ChristerpherHunter/JavaScript/lab_04/index.js
@@ -3,22 +3,13 @@ let taskContent = document.querySelector("#content")
 let taskTopic = document.querySelector("#subTitle")
 let taskTitle = document.querySelector("#title")
 let createTask = document.querySelector("#submit")
+let outPutTask = document.querySelector("#madeTask")
+let templateCard = document.querySelector("#templateCard")
 
 
 
-let toDos = [
-
+let toDos = [   
 ]
-
-
-// Listen for the click event
-createTask.addEventListener("click", () => {
-    createTodo(taskTitle.value, taskTopic.value, taskContent.value)
-    // Clears the field
-    taskContent.value = ""
-    taskTopic.value = ""
-    taskTitle.value = ""
-})
 
 function createTodo(title, topic, body) {
     let createTodoTask = {
@@ -26,8 +17,29 @@ function createTodo(title, topic, body) {
         topic: topic,
         body: body
     }
+    // console.log(createTodoTask)
 
     toDos = toDos.concat(createTodoTask)
+    // console.log(toDos)
 }
 
-console.log(toDos)
+// Listen for the click event
+createTask.addEventListener("click", () => {
+    createTodo(taskTitle.value, taskTopic.value, taskContent.value)
+    // console.log(taskContent.value, taskTopic.value, taskTitle.value)
+    // Clears the field
+    taskContent.value = ""
+    taskTopic.value = ""
+    taskTitle.value = ""
+    // console.log(toDos)
+})
+
+function showToDoItem(itemAdded) {
+
+    let newTodoItem = document.createElement("div")
+
+    let templateCardThing = templateCard.textContent.cloneNode(true) 
+
+    let 
+
+}
