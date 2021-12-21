905664d19440edd3d870b7827b4ae55f6738852d
James Johnson <jamesjhnsn1995@gmail.com>
2021-12-20 20:46:14 -0800

    added next page button

--- Code/James/javascript-labs/javascript-lab05/static/js/index.js
+++ Code/James/javascript-labs/javascript-lab05/static/js/index.js
@@ -3,6 +3,7 @@ let button = document.querySelector("#button-quote"),
   h1 = document.querySelector("#result"),
   h3 = document.querySelector("#author"),
   pageNumber = 1,
+  quotesContainer = document.querySelector("#quotes-container"),
   quotesList = [],
   i = 0;
 
@@ -25,10 +26,10 @@ button2.addEventListener("click", () => {
     h3.innerHTML = quotesList[i].author;
     h1.innerHTML = quotesList[i].body;
     console.log(i);
-    // quotesList = []
-  } 
-  
-//   else set i back to 0 and empty the quotes list then run the request function to start over with new list 
+    pageNumber += 1
+  }
+
+//   else set i back to 0 and empty the quotes list then run the request function to start over with new list
   else {
     i = 0;
     quotesList = [];
@@ -47,18 +48,24 @@ function getQuote() {
     })
 
     .then((response) => {
-    //   set i back to zero when function is called outside of the button 2 function
-     i = 0;
+      //   set i back to zero when function is called outside of the button 2 function
+      i = 0;
       quotesList = [];
       let quotes = response.data.quotes;
       let data = response.data;
       console.log(data);
       console.log(quotes);
-      quotes.forEach((quote) => {
-        quotesList.push(quote);
-        h3.innerHTML = quotes[i].author;
-        h1.innerHTML = quotes[i].body;
-      });
+      quotesList = quotes;
+      h3.innerHTML = quotes[0].author;
+      h1.innerHTML = quotes[0].body;
     })
+    // .then((result) => {
+    //   for (let i = 0; i < quotesList.length; i++) {
+    //     let quoteDiv = document.createElement("div");
+    //     quoteDiv.innerHTML = `<h1 id='body'>${quotesList[i].body}</h1><h3 id="author">${quotesList[i].author}</h3>`;
+
+    //     quotesContainer.appendChild(quoteDiv);
+    //   }
+    // })
     .catch((error) => console.log("error!", error));
 }
