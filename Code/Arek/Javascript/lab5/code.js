ebcf8aca77fa75016266887cd2d4f3f77bd60587
arek8675 <smullenarek10@gmail.com>
2021-12-20 19:56:27 -0800

    Finished lab5

--- Code/Arek/Javascript/lab5/code.js
+++ Code/Arek/Javascript/lab5/code.js
@@ -3,49 +3,92 @@
 
 
 
-let pageNumber = 1
-let queryString = 'astronomy'
+
+let queryString = document.getElementById('filterType').value
 let url = "https://favqs.com/api/quotes"
-
-
-
-
+let pageNumber = 0
+let quoteNum = 0
+let back = document.querySelector('#back-page')
+let next = document.querySelector('#next-page')
 let btn = document.querySelector('#button1')
-btn.addEventListener('click', function(){
+let lastPage = false
 
-    function displayQuote(data){
+function displayQuote(data){
+    console.log(data.quotes.length) - 1
+    let pagenum = document.getElementById('page-number')
+    let quote = document.getElementById('quote-div')
+    
+    pagenum.innerHTML = `
+        <article>Page Number: ${pageNumber}</article>
+        <article>Last Page = ${data.last_page}</article>
         
-            let quote = document.getElementById('quote-div')
-            let newQuote = document.createElement('h1')
-            
-            newQuote.innerText = `
-            "${data.quotes[Math.floor(Math.random()* data.quotes.length)].body}"\n\n
-             
-            `
-            quote.appendChild(newQuote) 
-        }
+    `
+        
+    quote.innerHTML = `
+        <h1>${data.quotes[quoteNum].body}\n</h1>
+        
+         
+        `
+    quoteNum += 1
+    
+    
+
+      
+}
+
+function apiCall(){
     axios({
         method: 'get',
         url: url,
         headers: {
-            Authorization: `Token token=${FAVQS_API_KEY}`
-        },
-        params: {
-            page: pageNumber,
-            filter: queryString
-        }
+        Authorization: `Token token=${FAVQS_API_KEY}`
+    },
+    params: {
+        page: pageNumber,
+        filter: queryString
+    }
             
     })
     .then(function (response){
-        console.log(response.data.quotes[Math.floor(Math.random()* response.data.quotes.length)].body)
+        console.log(response.data)
         displayQuote(response.data)
+        return response.data
         
     })
     .catch(function (error){
-            console.log(error)
+        console.log(error)
     })
+}
 
-})  
+
+btn.addEventListener('click', function(){
+    
+    console.log(queryString)
+    apiCall()
+
+    
+})
+
+back.addEventListener('click', function(){
+    if (pageNumber === 0){
+        stop()
+    }
+    else{
+        quoteNum = 0
+        pageNumber -= 1
+        apiCall()
+    }
+    
+})
+next.addEventListener('click', function(){
+    quoteNum = 0
+    pageNumber += 1
+    apiCall()
+})
+
+
+
+
 
 
 
