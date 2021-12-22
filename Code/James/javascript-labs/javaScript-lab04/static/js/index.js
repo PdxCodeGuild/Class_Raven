3b90d449b6bac518b94549f2e7d93070fa224716
Christerpher Hunter <djhunter67@gmail.com>
2021-12-21 20:53:51 -0500

    continuing the merge

--- Code/James/javascript-labs/javaScript-lab04/static/js/index.js
+++ Code/James/javascript-labs/javaScript-lab04/static/js/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let trash = document.querySelector("trash");
 
 let completeTodoSection = document.querySelector("#complete-todos"),
@@ -112,3 +113,23 @@ function updateTodoList() {
 }
 
 updateTodoList();
+=======
+let trash = document.getElementById('trash')
+let container = document.getElementById('.container')
+let add = document.getElementById('add')
+
+// trash.addEventListener('click', function(){
+//     trash.classList.add('grow')
+// })
+
+// trash.addEventListener('click', function (){
+//     let list = document.createElement('div')
+//     list.classList.add('list')
+// })
+
+
+add.addEventListener('click', function (){
+    var txt = prompt('Task to do is..');
+    add.innerHTML += '<li class=list-group-item'> + txt + '</li>'
+})
+>>>>>>> 5b26694b (prog)
