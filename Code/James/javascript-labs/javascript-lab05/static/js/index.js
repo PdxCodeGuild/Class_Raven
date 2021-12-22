202e9bd99371eecfb3bb54499ec37c5393632d56
James Johnson <jamesjhnsn1995@gmail.com>
2021-12-21 20:39:36 -0800

    added pagination

--- Code/James/javascript-labs/javascript-lab05/static/js/index.js
+++ Code/James/javascript-labs/javascript-lab05/static/js/index.js
@@ -1,55 +1,84 @@
 let button = document.querySelector("#button-quote"),
-  button2 = document.querySelector("#button-page"),
+  button2 = document.querySelector("#button-forward"),
+  button3 = document.querySelector("#button-back"),
   h1 = document.querySelector("#result"),
   h3 = document.querySelector("#author"),
   pageNumber = 1,
   quotesContainer = document.querySelector("#quotes-container"),
+  queryStringInput = document.getElementById('user-input'),
+  currentPage = document.querySelector('#current-page'),
+  button4 = document.querySelector("#button-search")
   quotesList = [],
+  
   i = 0;
-
-const url = `https://favqs.com/api/quotes?page=${pageNumber}`;
-
-const headers = {
-  Accept: "application/json",
-  Authorization: `Token token=${FAVQS_API_KEY}`,
-};
-
-button.addEventListener("click", () => {
-  getQuote();
-});
-
-button2.addEventListener("click", () => {
-  i += 1;
-
-//   if i is less than the total list length run this code black
-  if (i < 25) {
-    h3.innerHTML = quotesList[i].author;
-    h1.innerHTML = quotesList[i].body;
-    console.log(i);
-    pageNumber += 1
-  }
-
-//   else set i back to 0 and empty the quotes list then run the request function to start over with new list
-  else {
-    i = 0;
-    quotesList = [];
+  
+  
+  const headers = {
+    Accept: "application/json",
+    Authorization: `Token token=${FAVQS_API_KEY}`,
+  };
+  
+  button.addEventListener("click", () => {
+    params.page = 1;
     getQuote();
-  }
-});
-
-const params = {
-  page: pageNumber,
-};
-function getQuote() {
-  axios
+  });
+  
+  button4.addEventListener("click", () => {
+    // params.filter = queryString.value;
+    getQuote(pageNumber, queryStringInput.value);
+  });
+  
+  button3.addEventListener("click", () => {
+    i -= 1;
+    if (i > 0){
+      h3.innerHTML = quotesList[i].author;
+      h1.innerHTML = quotesList[i].body;
+      console.log(i);
+      
+    }
+    else {
+      i = 0
+      h3.innerHTML = quotesList[i].author;
+      h1.innerHTML = quotesList[i].body;
+      console.log(i);
+    }
+  });
+  
+  button2.addEventListener("click", () => {
+    i += 1;
+    
+    //   if i is less than the total list length run this code black
+    if (i < 25) {
+      h3.innerHTML = quotesList[i].author;
+      h1.innerHTML = quotesList[i].body;
+      console.log(i);
+    }
+    
+    //   else set i back to 0 and empty the quotes list then run the request function to start over with new list
+    else if (i === 25) {
+      params.page++;
+      pageNumber++;
+      getQuote(pageNumber);
+    }
+  });
+  
+  function getQuote(pageNumber, queryStringInput) {
+    let params = {
+      page: pageNumber,
+      filter: queryStringInput
+    };
+    
+    const url = `https://favqs.com/api/quotes`;
+    axios
     .get(url, {
       headers: headers,
       params: params,
     })
-
+    
     .then((response) => {
       //   set i back to zero when function is called outside of the button 2 function
       i = 0;
+      
       quotesList = [];
       let quotes = response.data.quotes;
       let data = response.data;
@@ -60,12 +89,12 @@ function getQuote() {
       h1.innerHTML = quotes[0].body;
     })
     // .then((result) => {
-    //   for (let i = 0; i < quotesList.length; i++) {
-    //     let quoteDiv = document.createElement("div");
-    //     quoteDiv.innerHTML = `<h1 id='body'>${quotesList[i].body}</h1><h3 id="author">${quotesList[i].author}</h3>`;
-
-    //     quotesContainer.appendChild(quoteDiv);
-    //   }
+      //   for (let i = 0; i < quotesList.length; i++) {
+        //     let quoteDiv = document.createElement("div");
+        //     quoteDiv.innerHTML = `<h1 id='body'>${quotesList[i].body}</h1><h3 id="author">${quotesList[i].author}</h3>`;
+        
+        //     quotesContainer.appendChild(quoteDiv);
+        //   }
     // })
     .catch((error) => console.log("error!", error));
 }
