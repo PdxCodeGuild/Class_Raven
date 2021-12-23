309d065b76999e9eaf940204cf9e7b6aa5d727fe
cdmichaelb <broetjem@gmail.com>
2021-12-22 19:26:12 -0800

    fixes to styling

--- Code/Michael/javascript/lab05/assets/javascript/index.js
+++ Code/Michael/javascript/lab05/assets/javascript/index.js
@@ -1,5 +1,16 @@
 /* Use the favqs.com api to show a random quote. To start, use https://favqs.com/api/qotd to GET a quote, then display it on the page.
 The API also supports browsing quotes. */
+let debugging = false;
+let quoteNumber = 0;
+let pageNumber = 1;
+let timer1;
+//if ctrl + shift + i is pressed, debugging will be toggled.
+window.addEventListener("keydown", (event) => {
+	if (event.code === "KeyI" && event.shiftKey) {
+		debugging = !debugging;
+		console.log("Debugging is now " + debugging);
+	}
+});
 
 const headers = {
 	Accept: "application/json",
@@ -18,31 +29,51 @@ function backgroundImages() {
 	}
 
 	let random = Math.floor(Math.random() * 10000) + 1;
-	console.log(random);
 
 	const url = `https://source.unsplash.com/random/${width}x${height}?sig=${random}&orientation=${deviceOrientation}&fit=fillmax&fill=blur`;
 	fetch(url).then((data) => {
 		let image = data.url;
-		console.log(image);
 		document.body.style.backgroundImage = `url(${image})`;
 	});
 }
 
 function randomQuote() {
-	fetch("https://favqs.com/api/qotd", { headers })
-		.then((response) => response.json())
-		.then((data) => {
-			const author = data.quote.author;
-			const quoteText = data.quote.body;
-			const quoteLink = data.quote.url; // Link to the quote on favqs.com, will use later maybe.
-			const quoteHTML = `<p><h4>${quoteText}</h4></p><p><i>&nbsp;-&nbsp;${author}</i></p>`;
-			document.getElementById("quote").innerHTML = quoteHTML;
-			setTimeout(randomQuote, delayQuote());
-			backgroundImages();
-			//randomBackground(); // Disabled because of lack of API key.
-		});
-}
+	clearTimeout(timer1);
+	if (focus) {
+		fetch("https://favqs.com/api/qotd", { headers })
+			.then((response) => response.json())
+			.then((data) => {
+				debugging ? console.log(focus) : null;
 
+				const author = data.quote.author;
+				const quoteText = data.quote.body;
+				const quoteLink = data.quote.url; // Link to the quote on favqs.com, will use later maybe.
+				const quoteHTML = `<p><h4>${quoteText}</h4></p><p><i>&nbsp;-&nbsp;${author}</i></p>`;
+				document.getElementById("quote").innerHTML = quoteHTML;
+
+				timer1 = setTimeout(randomQuote, delayQuote());
+				backgroundImages();
+			});
+	} else {
+		clearTimeout(timer1);
+	}
+}
+let focus = true;
+//Pause randomQuote function if window is not active.
+window.onblur = function () {
+	debugging ? console.log("Window is not active.") : null;
+	focus = false;
+	clearTimeout(timer1);
+};
+
+//Resume randomQuote function if window is active.
+window.onfocus = function () {
+	debugging ? console.log("Window is active.") : null;
+	focus = true;
+	clearTimeout(timer1);
+	randomQuote();
+};
+randomQuote();
 function promiseTest() {
 	let width = window.innerWidth;
 	let height = window.innerHeight;
@@ -92,47 +123,127 @@ function randomBackground() {
 		.then((response) => response.json())
 		.then((data) => {
 			const image = data.urls.raw;
-			console.log(data);
 			document.body.style.backgroundImage = `url(${image}&orientation=${deviceOrientation}&fit=fillmax&fill=blur&w=${width}&h=${height})`;
-			console.log(document.body.style.backgroundImage);
 		});
 }
 
 function delayQuote() {
 	const wpm = 180;
 	const quote = document.getElementById("quote").innerHTML;
-	console.log(document.getElementById("quote"), "test");
 	const quoteLength = quote.length;
 	const words = quote.split(" ");
 	const wordCount = words.length;
 	const avgWordLength = quoteLength / wordCount;
+
 	const delay = (quoteLength / avgWordLength / wpm) * 60000 + 2500;
+	debugging ? console.log(delay) : null;
 	return delay;
 }
 
-function searchQuotes() {
-	const searchTerm = document.getElementById("search").value;
-	fetch(`https://favqs.com/api/quotes?filter=${searchTerm}`, { headers })
+function searchQuotes(quoteNumber, pageNumber) {
+	// Disable elements while fetching.
+	document.getElementById("search-box").disabled = true;
+	document.getElementById("search-box").style.cursor = "not-allowed";
+	document.getElementById("search").disabled = true;
+	document.getElementById("search").style.cursor = "not-allowed";
+	document.getElementById("random-btn").disabled = true;
+	document.getElementById("random-btn").style.cursor = "not-allowed";
+	document.getElementById("left-btn").disabled = true;
+	document.getElementById("left-btn").style.cursor = "not-allowed";
+	document.getElementById("right-btn").disabled = true;
+	document.getElementById("right-btn").style.cursor = "not-allowed";
+
+	const searchTerm = document.getElementById("search-box").value;
+	fetch(
+		`https://favqs.com/api/quotes?filter=${searchTerm}&page=${pageNumber}`,
+		{
+			headers,
+		}
+	)
 		.then((response) => response.json())
 		.then((data) => {
-			const author = data.quotes.author;
-			const quoteText = data.quotes.body;
-			const quoteLink = data.quotes.url; // Link to the quote on favqs.com, will use later maybe.
+			document.getElementById("search-box").disabled = false;
+			document.getElementById("search-box").style.cursor = "auto";
+			document.getElementById("search").disabled = false;
+			document.getElementById("search").style.cursor = "auto";
+			if (quoteNumber > data.quotes.length) {
+				quoteNumber = data.quotes.length;
+			}
+			const author = data.quotes[quoteNumber].author;
+			const quoteText = data.quotes[quoteNumber].body;
+			const quoteLink = data.quotes[quoteNumber].url; // Link to the quote on favqs.com, will use later maybe.
 			const quoteHTML = `<p><h4>${quoteText}</h4></p><p><i>&nbsp;-&nbsp;${author}</i></p>`;
-			document.getElementById("quote-result").innerHTML = quoteHTML;
-			console.log(data);
+			debugging ? console.log(data) : null;
 
+			if (data.quotes.length === 1) {
+				document.getElementById("quote-result").innerHTML =
+					"<p><h1>No quotes found.</h1></p>";
+			} else {
+				document.getElementById("quote-result").innerHTML = quoteHTML;
+				document.getElementById("random-btn").disabled = false;
+				document.getElementById("random-btn").style.cursor = "auto";
+			}
+			if (data.last_page === false) {
+				//Enable right-btn
+				document.getElementById("right-btn").disabled = false;
+				document.getElementById("right-btn").style.cursor = "auto";
+			} else {
+				//Disable right-btn
+				document.getElementById("right-btn").disabled = true;
+				document.getElementById("right-btn").style.cursor = "not-allowed";
+			}
+
+			if (data.page === 1) {
+				//Disable left-btn
+				document.getElementById("left-btn").disabled = true;
+				document.getElementById("left-btn").style.cursor = "not-allowed";
+			} else {
+				//Enable left-btn
+				document.getElementById("left-btn").disabled = false;
+				document.getElementById("left-btn").style.cursor = "auto";
+			}
+			pageNumber = data.page;
 			backgroundImages();
 			//randomBackground(); // Disabled because of lack of API key.
 		});
 }
 
+// Event listeners
+document.getElementById("search-box").addEventListener("keydown", (event) => {
+	if (event.code === "Enter") {
+		(quoteNumber = 0), (pageNumber = 1);
+		searchQuotes(quoteNumber, pageNumber);
+	}
+});
 // Event Listener for Search
 document.getElementById("search").addEventListener("click", function (event) {
-	searchQuotes();
+	(quoteNumber = 0), (pageNumber = 1);
+	searchQuotes(quoteNumber, pageNumber);
+});
+// Event Listener for Random
+document
+	.getElementById("random-btn")
+	.addEventListener("click", function (event) {
+		//Random Number
+		quoteNumber = Math.floor(Math.random() * 24);
+		debugging ? console.log(quoteNumber) : null;
+		searchQuotes(quoteNumber, pageNumber);
+	});
+// Event Listener for Next
+document
+	.getElementById("right-btn")
+	.addEventListener("click", function (event) {
+		pageNumber++;
+		debugging ? console.log(pageNumber) : null;
+		searchQuotes(quoteNumber, pageNumber);
+	});
+// Event Listener for Previous
+document.getElementById("left-btn").addEventListener("click", function (event) {
+	pageNumber--;
+	debugging ? console.log(pageNumber) : null;
+	searchQuotes(quoteNumber, pageNumber);
 });
 
 //promiseTest();  This fuction works, but because of API limitations and lack of API keys, I'm not using it.
 
-randomQuote();
 backgroundImages();
