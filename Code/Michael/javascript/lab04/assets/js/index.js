7dce886c681a6cfd94843896f3f0d7695750ea19
cdmichaelb <broetjem@gmail.com>
2021-12-27 21:32:25 -0800

    final commit

--- Code/Michael/javascript/lab04/assets/js/index.js
+++ Code/Michael/javascript/lab04/assets/js/index.js
@@ -1,70 +1,90 @@
-//Add item to the list.
 function addItem(item) {
-	//Do not add item if exists in a list or if it is empty.
-	var li = document.getElementById("li-" + item); //Attempt to get item.
-	var li2 = document.getElementById("completed-li-" + item); //Attempt to get item.
+	// Add item to todo-list.
+	// Do not add item if it already exists on either list or is empty.
+	var li = document.getElementById("li-" + item); // Attempt to get item.
+	var li2 = document.getElementById("completed-li-" + item); // Attempt to get item.
 	if (li2 != null || li != null || item == "") {
 		//If item exists in a list or if it is empty.
 		return; //Do not add item.
 	}
-
-	var ul = document.getElementById("todo-list");
-	var li = document.createElement("li");
-	var label = document.createElement("label");
-
+	var ul = document.getElementById("todo-list"); // Get todo-list.
+	var li = document.createElement("li"); // Create li element.
+	var label = document.createElement("label"); // Create label element.
 	li.setAttribute("id", "li-" + item); //Set id of item.
-	li.setAttribute("onclick", "removeItem(this.id)"); //Set onclick attribute.
 	var checkbox = document.createElement("input"); //Create checkbox.
 	label.innerHTML = "&nbsp;" + item; //Set label text.
 	label.setAttribute("for", "checkbox-" + item); //Set for attribute.
+	label.setAttribute("id", "label-" + item); //Set id attribute.
 	checkbox.setAttribute("type", "checkbox"); //Set type to checkbox.
 	checkbox.setAttribute("id", "checkbox-" + item); //Set id of checkbox.
 	checkbox.setAttribute("onclick", "markItem('li-" + item + "')"); //Add onclick attribute.
-
 	ul.appendChild(li); //Add item to list.
 	li.appendChild(checkbox); //Add checkbox to item.
 	li.appendChild(label); //Add label to item.
 }
 
-//Remove item from the list.
-function removeItem(item) {
-	var ul = document.getElementById("todo-list");
-	var li = document.getElementById(item);
-	ul.removeChild(li); //Remove item from list.
+function removeItem(list, item) {
+	// Remove item from list.
+	var ul = document.getElementById(list); // Get list.
+	var li = document.getElementById(item); // Get item.
+	ul.removeChild(li); // Remove item from list.
 }
 
-//Mark item as completed.
 function markItem(item) {
-	var ul = document.getElementById("todo-list");
-	var li = document.getElementById(item);
-	var label = li.getElementsByTagName("label")[0]; //Get label.
-	var lineThrough = document.createElement("s"); //Create line-through element.
-	lineThrough.innerHTML = label.innerHTML;
-	label.innerHTML = "";
-	label.appendChild(lineThrough); //Add line-through element to label.
-	ul.removeChild(li); //Remove item from list.
-	var ul2 = document.getElementById("completed-list");
-	li.setAttribute("id", "completed-" + item); //Set id of item.
-	li.removeAttribute("onclick"); //Remove onclick attribute.
-	ul2.appendChild(li); //Add item to completed list.
-	var checkbox = document.getElementById("checkbox-" + item.substring(3)); //Get checkbox but remove `li-` from item.
-	checkbox.remove(); //Remove checkbox.
+	// Mark item as completed.
+	var ul = document.getElementById("todo-list"); // Get todo-list.
+	var li = document.getElementById(item); // Get item.
+	var label = li.getElementsByTagName("label")[0]; // Get label.
+	var lineThrough = document.createElement("s"); // Create line-through element.
+	lineThrough.innerHTML = label.innerHTML; // Set innerHTML of line-through element to innerHTML of label.
+	lineThrough.setAttribute("id", "completed-" + item.substring(3)); //Set id of line-through element.
+	label.innerHTML = ""; // Remove label text.
+	ul.removeChild(li); // Remove li item from list.
+	var ul2 = document.getElementById("completed-list"); // Get completed list.
+	li.setAttribute("id", "completed-" + item); // Set id of li item.
+	ul2.appendChild(li); // Add li item to completed-list.
+	var checkbox = document.getElementById("checkbox-" + item.substring(3)); // Get checkbox but remove `li-` from item.
+	checkbox.remove(); // Remove checkbox.
+	li.appendChild(lineThrough); // Add line-through element to label.
 }
 
-//Listen for the form to be submitted.
 document.getElementById("button").addEventListener("click", function () {
-	var item = document.getElementById("task").value;
-	addItem(item);
+	// Add event listener to button.
+	var item = document.getElementById("task").value; //Get value of task input.
+	addItem(item); //Add item to list.
 });
-// Add item to the list when enter is pressed.
+
 document.getElementById("task").addEventListener("keyup", function (event) {
-	if (event.code === Enter) {
-		var item = document.getElementById("task").value;
-		addItem(item);
+	// Add event listener to input.
+	if (event.code === "Enter") {
+		// If enter key is pressed.
+		event.preventDefault(); //Prevent form from submitting with default.
+		document.getElementById("button").click(); //Call button click event.
 	}
 });
 
-// Disable right click menu.
-document.addEventListener("contextmenu", function (e) {
-	e.preventDefault(); //Disable right click menu.
-});
+document
+	.getElementById("todo-list")
+	.addEventListener("contextmenu", function (event) {
+		// Add event listener to todo-list.
+		event.preventDefault(); // Prevent default context menu.
+		removeItem("todo-list", "li-" + event.target.id.substring(6)); // Remove item from todo-list.
+	});
+
+document
+	.getElementById("completed-list")
+	.addEventListener("contextmenu", function (event) {
+		// Add event listener to completed-list.
+		event.preventDefault(); //Prevent default context menu.
+		if (event.target.id.includes("completed-li-")) {
+			// If item includes `completed-li-`.
+			removeItem("completed-list", event.target.id); // Remove item from completed-list.
+		} else {
+			// If item does not include `completed-li-`.
+			removeItem(
+				// Remove item from completed-list.
+				"completed-list",
+				"completed-li-" + event.target.id.substring(10)
+			);
+		}
+	});
