191d2cc00df4e66aefdebcab0426751c7a829158
Christerpher Hunter <djhunter67@gmail.com>
2021-12-29 23:18:42 -0500

    moderate progress on frontend only

--- Code/ChristerpherHunter/Django/lab_01/todo_list/urls.py
+++ Code/ChristerpherHunter/Django/lab_01/todo_list/urls.py
@@ -5,4 +5,10 @@ app_name = "todo_list"
 urlpatterns = [
     path("", views.index, name="index"),
     path("create/", views.create, name="create"),
+    path("update/", views.update, name="update"),
+    path("delete/", views.delete, name="delete"),
+    path("retrieve/", views.retrieve, name="retrieve"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("signup/", views.signup, name ="signup"),
 ]
--- Code/ChristerpherHunter/Django/lab_01/todo_list/views.py
+++ Code/ChristerpherHunter/Django/lab_01/todo_list/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
-dummy_iter = {
-    1: "one",
-    2: "two",
-    3: "three"
+context = {
+    "dummy_iter" : [1, 2, 3]
 }
 
 def index(request):
-    return render(request, "todo_list/index.html", dummy_iter)
+    return render(request, "todo_list/index.html", context)
 
 def create(request):
     return render(request, "todo_list/create.html")
 
+def update(request):
+    return render(request, "todo_list/update.html")
+    
+def delete(request):
+    return render(request, "todo_list/delete.html")
+
+def retrieve(request):
+    return render(request, "todo_list/retrieve.html")
+
+def signup(request):
+    return render(request, "todo_list/signup.html")
+
+def login(request):
+    return render(request, "todo_list/login.html")
+    
+def logout(request):
+    return render(request, "todo_list/logout.html")
