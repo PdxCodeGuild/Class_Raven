b5373a484c5a81f17825a9138ac5caf2490ba889
cdmichaelb <broetjem@gmail.com>
2021-12-29 20:11:15 -0800

    created new apps, up next is customuser

--- Code/Michael/django/lab01/lab01/settings.py
+++ Code/Michael/django/lab01/lab01/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    "lab01",
+    "save",
+    "todolist"
 ]
 
 MIDDLEWARE = [
@@ -116,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- Code/Michael/django/lab01/lab01/urls.py
+++ Code/Michael/django/lab01/lab01/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    
+    path("/", include("lab01.urls")),
+    path("/save/", include("save.urls")),
+    path("/todolist/", include("todolist.urls")),
 ]
