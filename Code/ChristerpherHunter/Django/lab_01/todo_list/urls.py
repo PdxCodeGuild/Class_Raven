32a7567f1e4bdfe0058c2122a682e4eee8f913c4
Christerpher Hunter <djhunter67@gmail.com>
2021-12-29 21:53:48 -0500

    debug push

--- Code/ChristerpherHunter/Django/lab_01/todo_list/urls.py
+++ Code/ChristerpherHunter/Django/lab_01/todo_list/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "todo_list"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
 ]
--- Code/ChristerpherHunter/Django/lab_01/todo_list/views.py
+++ Code/ChristerpherHunter/Django/lab_01/todo_list/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+dummy_iter = {
+    1: "one",
+    2: "two",
+    3: "three"
+}
 
 def index(request):
-    return render(request, "todo_list/index.html")
+    return render(request, "todo_list/index.html", dummy_iter)
+
+def create(request):
+    return render(request, "todo_list/create.html")
+
