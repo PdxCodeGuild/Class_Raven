eea167c338d7967536b2fa01a1bea405ff6eb6b5
cdmichaelb <broetjem@gmail.com>
2021-12-30 19:45:23 -0800

    Rebuilt project from scratch

---
+++ Code/Michael/django/lab01/lab01project/users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    pass
+
+def __str__(self):
+    return self.username
+
