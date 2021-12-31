7189da6079b23ad0be50554a9eed398301e3287d
Austen C. Myers-Flachson <austenc.id@pm.me>
2021-12-30 18:13:45 -0700

    commit to switch branch

--- Code/Austen/working/flask-02/app.py
+++ Code/Austen/working/flask-02/app.py
@@ -1,1 +1,11 @@
-from flask import Flask
+from flask import Flask, request
+from flask import render_template as render
+from string import ascii_lowercase, ascii_uppercase, punctuation, digits
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  charsets = ['lower case', 'upper case', 'special characters', 'digits']
+  return render('index.html', charsets=charsets)
