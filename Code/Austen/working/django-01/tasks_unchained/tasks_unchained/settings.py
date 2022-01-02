4a3e7cca87cef2a391c89bfbdb0192deae316eae
Austen C. Myers-Flachson <austenc.id@pm.me>
2022-01-01 19:56:00 -0700

    move secret key before push to remote

--- Code/Austen/working/django-01/tasks_unchained/tasks_unchained/settings.py
+++ Code/Austen/working/django-01/tasks_unchained/tasks_unchained/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from .secrets import KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dj@mik(xxdpx0hh=(x7e51zuqbf$^xduh^lqhbjoik(a2bkn8a'
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
