0ff06b033676377495b8ae65c9d59086e9d2387c
arek8675 <smullenarek10@gmail.com>
2022-01-03 19:25:44 -0800

    made code simpler with .get_or_create method and fixed issue of priotiry foreign key giving a dropdown

--- Code/Arek/Django/lab1/todo-lab/todo/myapp/views.py
+++ Code/Arek/Django/lab1/todo-lab/todo/myapp/views.py
@@ -1,44 +1,32 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import render, reverse, redirect
 
 from django.http import HttpResponseRedirect
 from .models import  Priority, Todoitem
 
-# Create your views here.
-
 
 def indexView(request):
- # need to reference the polls/views.py from the demo folder
-   
-   
-   
+
    items = Todoitem.objects.all()
    context = {
       'myitems': items
    }
     
-    
-    
-   return render(request, 'myapp/mytemplate.html', context ) #if i return more than one thing it just displays the whole html code
+   return render(request, 'myapp/mytemplate.html', context ) 
 
 def add_todo(request):
    form = request.POST
    select_choice = form['choice-field']
    new_todo = form['new-todo']
 
-   new_priority = Priority() #creating a new row for priority
-   new_todo_item = Todoitem() #creating a new todo item in the DB
-   
-   new_priority.name = select_choice
-   new_priority.save()
+   new_priority,created = Priority.objects.get_or_create(name=select_choice) #creating a new row for priority
+   # we put ,created next to new_priority, so that we only get the first part of the tuple
+   #.create also does .save() for you
+   #.get or create returns a tuple (obj, created=boolean)
+   new_todo_item = Todoitem.objects.create(text=new_todo, priority=new_priority)
 
-   new_todo_item.text = new_todo
- 
-   new_todo_item.save() #saving the new rows in the database
-
-
-   
-   return HttpResponseRedirect(reverse('myapp:indexView')) #this just redirects back to the main page
+   return redirect('myapp:indexView') #this just redirects back to the main page
    #to show all the todo items
+   # instead of Http response redirect you can just import redirect with django shortcuts
    
    
 
