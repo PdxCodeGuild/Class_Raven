e68a000252017cc0f1b8a1cac01d3d0d4dce57f3
Christerpher Hunter <djhunter67@gmail.com>
2022-01-04 00:28:42 -0500

    finally have some DB transactions going successfully

--- Code/ChristerpherHunter/Django/lab_01/todo_list/models.py
+++ Code/ChristerpherHunter/Django/lab_01/todo_list/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TodoItem(models.Model):
+
+    todo_name = models.CharField(max_length=30, default="none")
+
+    text = models.TextField(max_length=150)
+
+    created_date = models.DateTimeField(auto_now_add=True) 
+
+    RADIO_CHOICES = [
+        ("high", "High"),
+        ("medium", "Medium"),
+        ("low", "Low"),
+    ]
+
+    priority = models.CharField(
+        max_length=6, choices=RADIO_CHOICES, default='medium')
+
+
--- Code/ChristerpherHunter/Django/lab_01/todo_list/views.py
+++ Code/ChristerpherHunter/Django/lab_01/todo_list/views.py
@@ -1,65 +1,114 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import TodoItem
+from colorama import Fore as F
+
+R = F.RESET
+
 
 # Create your views here.
-context = {
-    "dummy_iter" : [1, 2, 3]
-}
+
+def sanitize(request, todo_name):
+    """Sanitize DB of duplicate titles"""
+    # if TodoItem.objects.count() 
+
 
 def index(request):
-    
-    if request.method == 'GET':
-        return render(request, "todo_list/index.html", context)
-    
-    form = request.POST
-    print(form)
+
+    items = TodoItem.objects.all().order_by('-created_date')
+
+    context = {
+        "items": items,
+    }
+
+    return render(request, "todo_list/index.html", context)
+
 
 def create(request):
-    
+
     if request.method == 'GET':
         return render(request, "todo_list/create.html")
-    
+
     form = request.POST
     print(form)
+
+    name = form["name"]
+    text = form["message"]
+    priority = form["priority"]
+
+    todo_item = TodoItem()
+    todo_item.todo_name = name
+    todo_item.text = text
+    todo_item.priority = priority
+
+    todo_item.save()
+
+    # context = {
+    #     "message": todo_item,
+    #     "priority": priority_item
+    # }
+
+    return render(request, "todo_list/create.html")
+
 
 def update(request):
-    
+
     if request.method == 'GET':
-        return render(request, "todo_list/update.html", context)
-    
+        return render(request, "todo_list/update.html")
+
     form = request.POST
     print(form)
-    
+
+
 def delete(request):
-    
+
     if request.method == 'GET':
+
+        items = TodoItem.objects.all()
+        context = {
+            "items": items
+        }
         return render(request, "todo_list/delete.html", context)
-    
+
     form = request.POST
     print(form)
+    todo_item = form["todo-delete"]
+
+    try:
+        todo_inst = TodoItem.objects.get(todo_name=todo_item)
+        todo_inst.delete()
+        print(f"{F.GREEN}Delete successful!{R}")
+    except:
+        print(f"{F.RED}failed to delete!{R}")
+
+    return render(request, "todo_list/delete.html")
+
 
 def completed(request):
-    
+
     if request.method == 'GET':
-        return render(request, "todo_list/completed.html", context)
-    
+        return render(request, "todo_list/completed.html")
+
     form = request.POST
     print(form)
+
 
 def signup(request):
-    
+
     if request.method == 'GET':
         return render(request, "todo_list/signup.html")
-    
+
     form = request.POST
     print(form)
 
+
 def login(request):
-    
+
     if request.method == 'GET':
         return render(request, "todo_list/login.html")
-    
+
     form = request.POST
     print(form)
-    
+
+
 def logout(request):
     return render(request, "todo_list/logout.html")
