7ac9271de6a86a6626ba309f9f6e51a42dea306b
arek8675 <smullenarek10@gmail.com>
2022-01-04 20:28:53 -0800

    saving changes

--- Code/Arek/Django/lab2/blog/blogApp/myapp/views.py
+++ Code/Arek/Django/lab2/blog/blogApp/myapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def login(request):
+    return render(request, 'myapp/userform.html')
+
+def register(request):
+    pass
+
+def profile(request):
+    pass
