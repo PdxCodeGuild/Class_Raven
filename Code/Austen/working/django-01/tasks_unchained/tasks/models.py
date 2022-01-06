c46a8bb1da6f472b06085ceecd6247b2706b0f09
Austen Myers-Flachson <austenc.id@pm.me>
2022-01-05 19:25:56 -0700

    complete django todo lab

--- Code/Austen/working/django-01/tasks_unchained/tasks/models.py
+++
@@ -1,9 +0,0 @@
-from django.db.models import Model, CharField, BooleanField
-
-# Create your models here.
-class Task(Model):
-  name = CharField(max_length=20)
-  status = BooleanField(default=False)
-  
-  def __str__(self):
-    return self.name
