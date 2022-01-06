90fa5357934b3b6483a0115d2059497018ac44c4
arek8675 <smullenarek10@gmail.com>
2022-01-05 20:40:23 -0800

    saving changes

--- Code/Arek/Django/lab2/blog/blogApp/myapp/models.py
+++ Code/Arek/Django/lab2/blog/blogApp/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    pass # abstract user already has the username and password fields
+
+
+    def __str__(self):
+        return self.username
--- Code/Arek/Django/lab2/blog/blogApp/myapp/urls.py
+++ Code/Arek/Django/lab2/blog/blogApp/myapp/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = 'myapp'
 urlpatterns = [
-    path('', views.login, name='login')
-
+    path('', views.login, name='login'),
+    path('register/', views.register_page, name='register_page'),
+    path('register/new/', views.register, name='register'),
+    path('profile/', views.profile, name='profile')
 ]
