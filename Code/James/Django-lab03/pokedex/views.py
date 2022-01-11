8a56ed2d8e49d322f28a82138bdf58d18ca5c908
James Johnson <jamesjhnsn1995@gmail.com>
2022-01-10 19:41:05 -0800

    made pokedex models and templates folder

--- Code/James/Django-lab03/pokedex/views.py
+++ Code/James/Django-lab03/pokedex/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+
+from .models import Pokemon, PokemonType
 
 # Create your views here.
+def home(request):
+    return render(request, 'pokedex/index.html')
