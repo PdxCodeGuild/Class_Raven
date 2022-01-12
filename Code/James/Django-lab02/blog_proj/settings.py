3f1d4e73c7f1c9f91a47a00f8232a111ad1ce74a
James Johnson <jamesjhnsn1995@gmail.com>
2022-01-12 10:24:54 -0800

    changing styling and views

--- Code/James/Django-lab02/blog_proj/settings.py
+++ Code/James/Django-lab02/blog_proj/settings.py
@@ -121,7 +121,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
-MEDIA_ROOT ='static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- Code/James/Django-lab02/users_app/forms.py
+++ Code/James/Django-lab02/users_app/forms.py
@@ -10,15 +10,15 @@ class UserForm(forms.ModelForm):
 
         fields = [
             'first_name',
-            'last_name'
+            'last_name',
         ]
 
 
         widgets = {
-            'first_name': forms.TextInput(attrs={'class':'form-control'}),
-            'last_name': forms.TextInput(attrs={'class':'form-control'}),
-            'username': forms.TextInput(attrs={'class':'form-control'}),
-            'password': forms.PasswordInput(attrs={'class':'form-control'})
+            'first_name': forms.TextInput(attrs={'id':'form_field'}),
+            'last_name': forms.TextInput(attrs={'id':'form_field'}),
+            'username': forms.TextInput(attrs={'id':'form_field'}),
+            'password': forms.PasswordInput(attrs={'id':'form_field'})
         }
 
 
