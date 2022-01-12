e4400b7dd75c9fa9e4682606c83be0c55856b72c
Keegan Good <keegood8@gmail.com>
2022-01-11 18:45:30 -0800

    populated database, added CartItem through table

--- 4 Django/demo/ecommerce/shop_app/models.py
+++ 4 Django/demo/ecommerce/shop_app/models.py
@@ -28,9 +28,32 @@ class Product(models.Model):
         return f"{self.title}"
 
 
+# This model will be used as a "through table" between Carts and their Products
+# to allow multiples of each product in a cart with the 'quantity' attribute
+class CartItem(models.Model):
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE, related_name='cart_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.PositiveIntegerField(default=0)
+
 class Cart(models.Model):
     owner = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='cart')
-    products = models.ManyToManyField(Product, related_name='carts', blank=True)
+    
+    # through=CartItem will leverage the 'cart' and 'product' ForeignKey fields
+    # to allow multiples of a product to be added to a cart
+    products = models.ManyToManyField(Product, through=CartItem, related_name='carts', blank=True)
+
+    # return the sum of the quantities of the cart_items in the cart
+    def item_count(self):
+        count = 0
+        for cart_item in self.cart_items.all():
+            count += cart_item.quantity
+
+        return count
+
+        # the above loop as a list comprehension :D
+        # return sum([cart_item.quantity for cart_item in self.cart_items.all()])
+
+        
 
     def __str__(self):
         return f"{self.owner}'s cart"
