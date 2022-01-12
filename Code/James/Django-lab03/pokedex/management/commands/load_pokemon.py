594d6103cb397ca6d236a0d16767bbf8db354d62
James Johnson <jamesjhnsn1995@gmail.com>
2022-01-11 20:13:19 -0800

    added command to load json into db

---
+++ Code/James/Django-lab03/pokedex/management/commands/load_pokemon.py
@@ -0,0 +1,39 @@
+from django.core.management.base import BaseCommand
+from pokedex.models import Pokemon, PokemonType
+import json
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        with open('./pokedex/management/commands/pokemon.json', 'r') as f:
+            contents = json.load(f)
+            pokemon_dict = contents['pokemon']
+            
+            
+            # f.read()
+            
+            for pokemon in pokemon_dict:
+                number = pokemon['number']
+                name = pokemon['name']
+                height = pokemon['height']
+                weight = pokemon['weight']
+                image_front = pokemon['image_front']
+                image_back = pokemon['image_back']
+                types = pokemon['types']
+
+                types, created = PokemonType.objects.get_or_create(name=types)
+
+
+                print(image_front)
+
+                pokemon = Pokemon.objects.create(
+                    number=number,
+                    name=name,
+                    height=height,
+                    weight=weight,
+                    image_front=image_front,
+                    image_back=image_back,
+                    # types=types,
+                )
+
+
+            
