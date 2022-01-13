a3a51c51e730985a32aedb28dc35a2814399110e
Philip Bartoo <Philip_99bartoo@hotmail.com>
2022-01-12 17:21:34 -0800

    Philip-Django-Lab01_Todo Added Priorities

--- Code/Philip/Django/Lab01_Todo/todo_project/todo_app/models.py
+++ Code/Philip/Django/Lab01_Todo/todo_project/todo_app/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.db.models.deletion import SET_DEFAULT
+from django.db.models.fields import CharField
+from django.db import models
 
-class Priority(models.Model): 
-    priority = models.CharField(max_length=7)
-   
+CHOICES=[
+    ('low','Low'),
+    ('medium','Medium'),
+    ('high','High'),
+]
+
+class Priority(models.Model):
+    priority= models.CharField(choices=CHOICES,max_length=20)
+
     def __str__(self):
         return self.priority
 
@@ -11,7 +20,7 @@ class Todo(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
     completed_date = models.DateTimeField(null=True, blank=True)
-    priority_choice = models.ForeignKey(Priority, on_delete=models.CASCADE, blank=True, null=True)
+    priority_choice = models.ForeignKey(Priority,on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
-        return self.title
+        return self.title + self.priority_choice
