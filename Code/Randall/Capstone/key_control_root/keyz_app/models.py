7316c87399ca1ef3cdcf6b6a49f1cbd4605479be
TheAncientBooer <randall.mayton@gmail.com>
2022-01-25 07:59:16 -0800

    capstone models

--- Code/Randall/Capstone/key_control_root/keyz_app/models.py
+++ Code/Randall/Capstone/key_control_root/keyz_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+class key_list(models.Model):
+    key_id = models.CharField(max_length = 100)
+    key_for = models.CharField(max_length = 100)   
+    available_status = models.BooleanField()
+
+class key_user(models.Model):
+    user_id = models.CharField(max_length= 100)
+    user_name = models.CharField(max_length= 100)  
+
+class key_issue(models.Model):
+    key_id = models.ForeignKey(key_list, on_delete = models.CASCADE)
+    user_id = models.ForeignKey(key_user, on_delete = models.CASCADE)
+    issue_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    user_sign_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    return_due_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    
+
+    
