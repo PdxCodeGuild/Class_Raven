d85fef899d71c58e588e0be1df3c5e20b3461d60
KG <keegood8@gmail.com>
2022-01-28 17:49:16 -0800

    added form handling with vue to create new polls

--- 4 Django/demo/django_polls_drf_vue/polls/views.py
+++ 4 Django/demo/django_polls_drf_vue/polls/views.py
@@ -66,67 +66,40 @@ def vote(request):
     return response
 
 
+@api_view(['POST'])
 def create_question(request):
+    response = Response()
 
-    print('user requesting to create a question:', request.user)
-
-    # the form data is available through the request object
-    form = request.POST
-
-    # grab the values from the form
-    question_text = form['question_text']
-    number_of_choices = int(form['number_of_choices'])
+    # grab the values from the request data
+    new_question_text = request.data.get('newQuestionText')
+    choices = request.data.get('choices')
 
     # create the new question in the database
-    new_question = Question()
-    # set the question text
-    new_question.question_text = question_text
+    new_question = Question.objects.create(
+        question_text = new_question_text,
+        user = request.user
+    )
 
-    # assign a user to the question
-    new_question.user = request.user
+    # create Choice objects for each choice in the list
+    for choice in choices:
+        Choice.objects.create(
+            choice_text = choice,
+            question = new_question
+        )
 
-    # save the object to the database
-    new_question.save()
+    questions = Question.objects.all().order_by('-pub_date')
 
-    # generate a list of choice numbers
-    choice_numbers = [number for number in range(1, number_of_choices + 1)]
+    # many=True allows multiple instances to be serialized
+    question_serializer = QuestionDetailSerializer(questions, many=True)
 
-    # gather data to render into the template
-    context = {
-        'choice_numbers': choice_numbers,
-        'question': new_question
+    response.data = {
+        'questions': question_serializer.data
     }
 
-    # render the add_choices.html template using the context data
-    return render(request, 'polls/add_choices.html', context)
+    return response
 
 
-
-def add_choices(request):
-    form = request.POST
-
-    # find the question object that has the question_id from the form
-    question = get_object_or_404(Question, id=form['question_id'])
-
-    for key in form:
-        if key.startswith('choice'):
-            # create a new Choice database object
-            new_choice = Choice()
-
-            # set the choice_text
-            # form['choice1'], form['choice2'], etc
-            new_choice.choice_text = form[key]
-
-            # associate the choice to the question
-            new_choice.question = question
-
-            # save the changes to the database
-            new_choice.save()
-
-    # redirect to the home page to view all the polls
-    return HttpResponseRedirect(reverse('polls:home'))
-
-
+# Extra challenge, convert this view to DRF API view
 def user_polls_list(request, username):
     user = get_object_or_404(CustomUser, username=username)
     questions = Question.objects.filter(user=user)
