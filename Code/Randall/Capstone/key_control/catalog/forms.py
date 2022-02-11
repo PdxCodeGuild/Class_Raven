e469c851dbf825c878f0ccb19a2cd8ed221b917b
TheAncientBooer <randall.mayton@gmail.com>
2022-02-10 18:39:26 -0800

    fixed broken form

--- Code/Randall/Capstone/key_control/catalog/forms.py
+++ Code/Randall/Capstone/key_control/catalog/forms.py
@@ -1,8 +1,23 @@
 from django import forms
+import datetime 
+from django.core.exceptions import ValidationError
+
+
 
 class RenewKeyForm(forms.Form):
     #Form for admin to renew keys.
-    due_date = forms.DateField(
-            help_text="Enter key return date.")
+    renewal_date = forms.DateField(help_text="Enter new due date.")
 
-   # widget = forms.DateInput()
+    def clean_renewal_date(self):
+        data = self.cleaned_data['renewal_date']
+
+        """# Check if not in past.       #Add back later after testing
+        if data < datetime.date.today():
+            raise ValidationError(('Invalid date - renewal in past'))
+        """    
+        # Makes sure date doesn't exceed the allowed specified
+        if data > datetime.date.today() + datetime.timedelta(weeks=4):
+            raise ValidationError(('Error: Date entered exceeds 4 weeks, try again.'))
+
+        # Remember to always return the cleaned data.
+        return data
